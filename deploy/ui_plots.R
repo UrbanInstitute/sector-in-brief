1809c206fee2d6572655c0356274f9f029e9628b
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-12 01:10:59 -0400

    Redundant

--- deploy/ui_plots.R
+++ deploy/ui_plots.R
@@ -11,14 +11,9 @@ num_nonprofits <- data |>
   ) |>
   dplyr::collapse()
 num_nonprofits_table <- DT::datatable(num_nonprofits, rownames = FALSE)
-num_nonprofits_plot <- plotly::plot_ly(
-  num_nonprofits,
-  x = ~YEAR,
-  y = ~`Number of Nonprofits`,
-  type = "scatter",
-  mode = "lines",
-  name = "Number of Nonprofits"
-)
+num_nonprofits_plot <- 
+  ggplot2::ggplot(num_nonprofits, ggplot2::aes(x = YEAR, y = `Number of Nonprofits`)) +
+  ggplot2::geom_line()
 
 # Grouped by Subsector
 num_nonprofits_subsector <- data |>
@@ -27,7 +22,7 @@ num_nonprofits_subsector <- data |>
     `Number of Nonprofits` = sum(num_nonprofit, na.rm = TRUE),
   ) |>
   dplyr::filter(
-    NTEE_INDUSTRY_GROUP %in% c("HMS", "EDU", "ART")
+    NTEE_INDUSTRY_GROUP %in% c("HMS", "EDU", "ART", "PSB", "HEL")
   ) |>
   dplyr::collapse()
 num_nonprofits_subsector_plot <- plotly::plot_ly(
