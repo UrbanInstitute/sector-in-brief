e5844013a2b273d1aafa624a84496f8553ca46eb
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-16 13:42:37 -0400

    Swap positions of frontens pre-refactoring

--- deploy/frontend.R
+++ deploy/frontend.R
@@ -1,177 +1,6 @@
 source("R/org_filters.R")
 
 # Consolidate frontends
-
-daf_frontend <- bslib::nav_panel(
-  title = "Donor Advised Funds",
-  div(
-    br(),
-    h2("Donor Advised Funds", class = "pageheader"),
-    br(),
-    h3("A donor advised fund (DAF) is a tool that allows individuals and organizations to contribute money and non-cash assets to a giving account, receive an immediate tax deduction, and recommend grants to nonprofits at a later time."),
-    br()
-  ),
-  bslib::card(
-    card_header("Step 1: Filters"),
-    title = "",
-    bslib::layout_columns(
-      bslib::card(
-        card_header("Organization Type"),
-        org_filters$daf_org_level,
-        shiny::conditionalPanel(
-          org_filters$daf_other_orgs,
-          condition = "input.daf_org_level == 'Other Nonprofits'"
-        )
-      ),
-      bslib::card(
-        card_header("Geography"),
-        geo_filter_ui("daf_geo_filter", state_choices),
-      ),
-      bslib::card(
-        bslib::card_header("Subsector"),
-        shiny::radioButtons(
-          inputId = "daf_subsector_level",
-          label = NULL,
-          inline = TRUE,
-          choices = list(
-            "All Subsectors" = "all", 
-            "Individual Subsectors" = "individual"
-          )
-        ),
-        shiny::conditionalPanel(
-          selectizeInput(
-            inputId = "daf_subsector_select",
-            label = NULL,
-            choices = list(
-              "Arts, Culture, and Humanities" = "ART", 
-              "Education (minus Universities)" = "EDU",
-              "Health (minus Hospitals)" = "HEL",
-              "Human Services" = "HMS",
-              "International, Foreign Affairs" = "IFA",
-              "Public, Societal Benefit" = "PSB",
-              "Religion Related" = "REL",
-              "Mutual/Membership Benefit" = "MMB",
-              "Universities" = "UNI",
-              "Hospitals" = "HOS"
-            ),
-            multiple = TRUE,
-            options = list(maxItems = 5)
-          ),
-          condition = "input.daf_subsector_level == 'individual'"
-        )
-      ),
-      bslib::card(
-        card_header("Asset Size"),
-        shiny::radioButtons(
-          inputId = "daf_size_level",
-          label = NULL,
-          inline = TRUE,
-          choices = list(
-            "All Asset Sizes" = "all", 
-            "Individual Asset Sizes" = "individual"
-          )
-        ),
-        shiny::conditionalPanel(
-          selectizeInput(
-            inputId = "daf_size_select",
-            label = NULL,
-            multiple = TRUE,
-            options = list(maxItems = 5),
-            choices = list(
-              "Under $100,000" = 1,
-              "$100,000 - $499,999" = 2,
-              "$500,000 - $999,999" = 3,
-              "$1 Million - $4.99 Million" = 4,
-              "$5 Million - $9.99 Million" = 5,
-              "Above $10 Million" = 6
-            )
-          ),
-          condition = "input.daf_size_level == 'individual'"
-        )
-      )
-    ),
-    bslib::input_task_button(
-      id = "process_daf_data",
-      style = "border-radius: 0; font-size: 18px; color: #ffffff; margin: auto; background-color: #1696d2; border-color: #1696d2;",
-      label = "RETRIEVE DATA",
-      label_busy = "UPDATING PLOTS",
-      type = "primary"
-    )
-  ),
-  bslib::navset_card_tab(
-    title =   "View Results",
-    height = "100%",
-    bslib::nav_panel(
-      "Overall",
-      layout_column_wrap(
-        width = 1,
-        bslib::card(
-          bslib::card_body(plotOutput("daf_plot_overall")),
-          bslib::card_body(plotOutput("plot_overall_num_daf")),
-          daf_footer
-        ),
-        bslib::card(
-          bslib::card_body(reactable::reactableOutput("daf_table_overall")),
-          bslib::card_body(
-            downloadButton("downloadData", "DOWNLOAD", class = "btn-download", icon = NULL)
-          )
-        )
-      )
-    ),
-    bslib::nav_panel(
-      "By Subsector",
-      layout_column_wrap(
-        width = 1,
-        bslib::card(
-          bslib::card_body(plotOutput("daf_plot_subsector")),
-          bslib::card_body(plotOutput("plot_subsector_num_daf")),
-          daf_footer
-        ),
-        bslib::card(
-          bslib::card_body(reactable::reactableOutput("daf_table_subsector")),
-          bslib::card_body(
-            downloadButton("downloadData", "DOWNLOAD", class = "btn-download", icon = NULL)
-          )
-        )
-      )
-    ),
-    bslib::nav_panel(
-      "By Geography",
-      layout_column_wrap(
-        width = 1,
-        bslib::card(
-          bslib::card_body(plotOutput("daf_plot_geo")),
-          bslib::card_body(plotOutput("plot_geo_num_daf")),
-          daf_footer
-        ),
-        bslib::card(
-          bslib::card_body(reactable::reactableOutput("daf_table_geo")),
-          bslib::card_body(
-            downloadButton("downloadData", "DOWNLOAD", class = "btn-download", icon = NULL)
-          )
-        )
-      )
-    ),
-    bslib::nav_panel(
-      "By Asset Size",
-      layout_column_wrap(
-        width = 1,
-        bslib::card(
-          bslib::card_body(plotOutput("daf_plot_size")),
-          bslib::card_body(plotOutput("plot_size_num_daf")),
-          daf_footer
-        ),
-        bslib::card(
-          bslib::card_body(reactable::reactableOutput("daf_table_size")),
-          bslib::card_body(
-            downloadButton("downloadData", "DOWNLOAD", class = "btn-download", icon = NULL)
-          )
-        )
-      )
-    )
-  )
-)
-
 num_nonprofit_frontend <-   bslib::nav_panel(
   title = "Number",
   div(
@@ -358,3 +187,175 @@ num_nonprofit_frontend <-   bslib::nav_panel(
     )
   )
 )
+
+
+daf_frontend <- bslib::nav_panel(
+  title = "Donor Advised Funds",
+  div(
+    br(),
+    h2("Donor Advised Funds", class = "pageheader"),
+    br(),
+    h3("A donor advised fund (DAF) is a tool that allows individuals and organizations to contribute money and non-cash assets to a giving account, receive an immediate tax deduction, and recommend grants to nonprofits at a later time."),
+    br()
+  ),
+  bslib::card(
+    card_header("Step 1: Filters"),
+    title = "",
+    bslib::layout_columns(
+      bslib::card(
+        card_header("Organization Type"),
+        org_filters$daf_org_level,
+        shiny::conditionalPanel(
+          org_filters$daf_other_orgs,
+          condition = "input.daf_org_level == 'Other Nonprofits'"
+        )
+      ),
+      bslib::card(
+        card_header("Geography"),
+        geo_filter_ui("daf_geo_filter", state_choices),
+      ),
+      bslib::card(
+        bslib::card_header("Subsector"),
+        shiny::radioButtons(
+          inputId = "daf_subsector_level",
+          label = NULL,
+          inline = TRUE,
+          choices = list(
+            "All Subsectors" = "all", 
+            "Individual Subsectors" = "individual"
+          )
+        ),
+        shiny::conditionalPanel(
+          selectizeInput(
+            inputId = "daf_subsector_select",
+            label = NULL,
+            choices = list(
+              "Arts, Culture, and Humanities" = "ART", 
+              "Education (minus Universities)" = "EDU",
+              "Health (minus Hospitals)" = "HEL",
+              "Human Services" = "HMS",
+              "International, Foreign Affairs" = "IFA",
+              "Public, Societal Benefit" = "PSB",
+              "Religion Related" = "REL",
+              "Mutual/Membership Benefit" = "MMB",
+              "Universities" = "UNI",
+              "Hospitals" = "HOS"
+            ),
+            multiple = TRUE,
+            options = list(maxItems = 5)
+          ),
+          condition = "input.daf_subsector_level == 'individual'"
+        )
+      ),
+      bslib::card(
+        card_header("Asset Size"),
+        shiny::radioButtons(
+          inputId = "daf_size_level",
+          label = NULL,
+          inline = TRUE,
+          choices = list(
+            "All Asset Sizes" = "all", 
+            "Individual Asset Sizes" = "individual"
+          )
+        ),
+        shiny::conditionalPanel(
+          selectizeInput(
+            inputId = "daf_size_select",
+            label = NULL,
+            multiple = TRUE,
+            options = list(maxItems = 5),
+            choices = list(
+              "Under $100,000" = 1,
+              "$100,000 - $499,999" = 2,
+              "$500,000 - $999,999" = 3,
+              "$1 Million - $4.99 Million" = 4,
+              "$5 Million - $9.99 Million" = 5,
+              "Above $10 Million" = 6
+            )
+          ),
+          condition = "input.daf_size_level == 'individual'"
+        )
+      )
+    ),
+    bslib::input_task_button(
+      id = "process_daf_data",
+      style = "border-radius: 0; font-size: 18px; color: #ffffff; margin: auto; background-color: #1696d2; border-color: #1696d2;",
+      label = "RETRIEVE DATA",
+      label_busy = "UPDATING PLOTS",
+      type = "primary"
+    )
+  ),
+  bslib::navset_card_tab(
+    title =   "View Results",
+    height = "100%",
+    bslib::nav_panel(
+      "Overall",
+      layout_column_wrap(
+        width = 1,
+        bslib::card(
+          bslib::card_body(plotOutput("daf_plot_overall")),
+          bslib::card_body(plotOutput("plot_overall_num_daf")),
+          daf_footer
+        ),
+        bslib::card(
+          bslib::card_body(reactable::reactableOutput("daf_table_overall")),
+          bslib::card_body(
+            downloadButton("downloadData", "DOWNLOAD", class = "btn-download", icon = NULL)
+          )
+        )
+      )
+    ),
+    bslib::nav_panel(
+      "By Subsector",
+      layout_column_wrap(
+        width = 1,
+        bslib::card(
+          bslib::card_body(plotOutput("daf_plot_subsector")),
+          bslib::card_body(plotOutput("plot_subsector_num_daf")),
+          daf_footer
+        ),
+        bslib::card(
+          bslib::card_body(reactable::reactableOutput("daf_table_subsector")),
+          bslib::card_body(
+            downloadButton("downloadData", "DOWNLOAD", class = "btn-download", icon = NULL)
+          )
+        )
+      )
+    ),
+    bslib::nav_panel(
+      "By Geography",
+      layout_column_wrap(
+        width = 1,
+        bslib::card(
+          bslib::card_body(plotOutput("daf_plot_geo")),
+          bslib::card_body(plotOutput("plot_geo_num_daf")),
+          daf_footer
+        ),
+        bslib::card(
+          bslib::card_body(reactable::reactableOutput("daf_table_geo")),
+          bslib::card_body(
+            downloadButton("downloadData", "DOWNLOAD", class = "btn-download", icon = NULL)
+          )
+        )
+      )
+    ),
+    bslib::nav_panel(
+      "By Asset Size",
+      layout_column_wrap(
+        width = 1,
+        bslib::card(
+          bslib::card_body(plotOutput("daf_plot_size")),
+          bslib::card_body(plotOutput("plot_size_num_daf")),
+          daf_footer
+        ),
+        bslib::card(
+          bslib::card_body(reactable::reactableOutput("daf_table_size")),
+          bslib::card_body(
+            downloadButton("downloadData", "DOWNLOAD", class = "btn-download", icon = NULL)
+          )
+        )
+      )
+    )
+  )
+)
+
