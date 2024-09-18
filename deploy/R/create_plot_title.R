d57ae41cde6a9a52a8c2be3d6ba2b5d70505cad4
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-18 05:35:04 -0400

    Add years to title if time series

--- deploy/R/create_plot_title.R
+++ deploy/R/create_plot_title.R
@@ -1,13 +1,15 @@
-create_plot_title <- function(org_level, other_orgs, date_range) {
+create_plot_title <- function(org_level, other_orgs, date_range, time_series) {
   if (org_level == "Other Nonprofits") {
     title <- paste("Number of", other_orgs)
   } else {
     title <- paste("Number of", org_level)
   }
-  if (date_range[1] != date_range[2] ) {
-    title <- paste(title, ",", date_range[1], "-", date_range[2])
-  } else {
-    title <- paste(title, ",", date_range[1])
+  if (time_series){
+    if (date_range[1] != date_range[2] ) {
+      title <- paste(title, ",", date_range[1], "-", date_range[2])
+    } else {
+      title <- paste(title, ",", date_range[1])
+    }
   }
   return(title)
 }
