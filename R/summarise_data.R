436746ea94af2bba16ad43c7f631f5c4ee279027
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-19 01:16:31 -0400

    Debug incorrect variable name and definition for asset size

--- R/summarise_data.R
+++ R/summarise_data.R
@@ -21,15 +21,15 @@ summarise_data <- function(data, groupby_var, sum_var, geo_level, subsector_leve
   }
   if (asset_size_level != "all") {
     table_by_asset_size <- data |>
-      dplyr::mutate(Asset_Size = case_when(
-        Asset_Size == 1 ~ "Under $100,000",
-        Asset_Size == 2 ~ "$100,000 - $499,999",
-        Asset_Size == 3 ~ "$500,000 - $999,999",
-        Asset_Size == 4 ~ "$1 Million - $4.99 Million",
-        Asset_Size == 5 ~ "$5 Million - $9.99 Million",
-        Asset_Size == 6 ~ "Above $10 Million",
+      dplyr::mutate("Asset Size" = case_when(
+        `Asset Size` <= 1 ~ "Under $100,000",
+        `Asset Size` == 2 ~ "$100,000 - $499,999",
+        `Asset Size` == 3 ~ "$500,000 - $999,999",
+        `Asset Size` == 4 ~ "$1 Million - $4.99 Million",
+        `Asset Size` == 5 ~ "$5 Million - $9.99 Million",
+        `Asset Size` == 6 ~ "Above $10 Million",
       )) |>
-      group_by(!!sym(groupby_var), Asset_Size) |>
+      group_by(!!sym(groupby_var), `Asset Size`) |>
       summarise(!!sum_var := sum(!!sym(sum_var), na.rm = TRUE)) |>
       dplyr::collapse()
     table_ls[["by_asset_size"]] <- table_by_asset_size
