c8acd9b25986dfffcd815fc2e7eb098efc24de72
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-19 00:48:34 -0400

    Update input names for clarity

--- R/geo_query.R
+++ R/geo_query.R
@@ -1,14 +1,14 @@
 # Single function to query geographic levels
 geo_query <- function(data, geo_level, region, state_single, state_mult, county, cbsa) {
-  if (geo_level == "census_region") {
-    data <- filter(data, census_region %in% region)
-  } else if (geo_level == "CENSUS_STATE_ABBR") {
-    data <- filter(data, CENSUS_STATE_ABBR %in% state_mult)
-  } else if (geo_level == "CENSUS_COUNTY_NAME") {
-    data <- filter(data, CENSUS_STATE_ABBR %in% state_single)
-    data <- filter(data, CENSUS_COUNTY_NAME %in% county)
-  } else if (geo_level == "CENSUS_CBSA_NAME") {
-    data <- filter(data, CENSUS_CBSA_NAME %in% cbsa)
+  if (geo_level == "Census Region") {
+    data <- filter(data, `Census Region` %in% region)
+  } else if (geo_level == "Census State") {
+    data <- filter(data, `Census State` %in% state_mult)
+  } else if (geo_level == "Census County") {
+    data <- filter(data, `Census State` %in% state_single)
+    data <- filter(data, `Census County` %in% county)
+  } else if (geo_level == "Census CBSA") {
+    data <- filter(data, `Census CBSA` %in% cbsa)
   }
   return(data)
 }
