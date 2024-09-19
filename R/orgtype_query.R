88db19cd57c20e9bc3e9589c203b1d1b38ce67e0
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-19 00:45:50 -0400

    Update column name

--- R/orgtype_query.R
+++ R/orgtype_query.R
@@ -1,13 +1,13 @@
 # Single function to query organization types
-orgtype_query <- function(data, org, other_orgs = NULL) {
+orgtype_query <- function(data, org, other_orgs = NULL, orgvar = "Organization Type") {
   if (org == "501(c)(3) Public Charities"){
-    data <- filter(data, Organization_Type == org)
+    data <- filter(data, !!sym(orgvar) == org)
   } else if (org == "501(c)(3) Private Foundations"){
-    data <- filter(data, Organization_Type == org)
+    data <- filter(data, !!sym(orgvar) == org)
   } else if (org == "501(c)(4) Social Welfare Organizations") {
-    data <- filter(data, Organization_Type == "501(c)(4)")
+    data <- filter(data, !!sym(orgvar) == "501(c)(4)")
   } else if (org == "Other Nonprofits") {
-    data <- filter(data, Organization_Type == other_orgs)
+    data <- filter(data, !!sym(orgvar) == other_orgs)
   }
   return(data)
 }
