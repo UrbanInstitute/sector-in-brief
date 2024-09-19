0bfd90b1e26b96892478113fcafd1a596193d775
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-19 18:53:16 -0400

    Avoid data masking

--- R/orgtype_query.R
+++ R/orgtype_query.R
@@ -1,13 +1,13 @@
 # Single function to query organization types
 orgtype_query <- function(data, org, other_orgs = NULL, orgvar = "Organization Type") {
   if (org == "501(c)(3) Public Charities"){
-    data <- filter(data, !!sym(orgvar) == org)
+    data <- dplyr::filter(data, `Organization Type` == org)
   } else if (org == "501(c)(3) Private Foundations"){
-    data <- filter(data, !!sym(orgvar) == org)
+    data <- dplyr::filter(data, `Organization Type` == org)
   } else if (org == "501(c)(4) Social Welfare Organizations") {
-    data <- filter(data, !!sym(orgvar) == "501(c)(4)")
+    data <- dplyr::filter(data, `Organization Type` == "501(c)(4)")
   } else if (org == "Other Nonprofits") {
-    data <- filter(data, !!sym(orgvar) == other_orgs)
+    data <- dplyr::filter(data, `Organization Type` == other_orgs)
   }
   return(data)
 }
