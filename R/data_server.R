b2ee3578a63d688f1871a0f6c1ef444bff36bbda
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-22 23:13:05 -0400

    Error message when incorrect inputs selected

--- R/data_server.R
+++ R/data_server.R
@@ -17,124 +17,144 @@ data_server <- function(id, geo_df, data, groupby_var, sum_var, single_plot_func
         input$size_select,
         input$date_range
       )
+
     })
     observeEvent(input$process_data, {
-      title <- create_plot_title(input$org_level, input$other_orgs, input$date_range, time_series, title_prefix)
-      subtitle <- create_plot_subtitle(
+      input_validation_msg <- validate_inputs(
+        input$org_level,
+        input$other_orgs,
         geo_filters$geo_level(),
         geo_filters$region_selector(),
-        geo_filters$state_selector_single(),
         geo_filters$state_selector_multi(),
+        geo_filters$state_selector_single(),
         geo_filters$county_selector(),
         geo_filters$cbsa_selector(),
         input$subsector_select,
-        input$size_filter
+        input$size_filter,
+        input$date_range[1],
+        input$date_range[2]
       )
-      shiny::withProgress(min = 1, max = 5, {
-        setProgress(1, message = "Filtering Data...")
-        filtered_data <- filter_data(
-          data = data,
-          org_level = input$org_level,
-          other_orgs = input$other_orgs,
-          geo_level = geo_filters$geo_level(),
-          region = geo_filters$region_selector(),
-          state_single = geo_filters$state_selector_single(),
-          state_mult = geo_filters$state_selector_multi(),
-          county = geo_filters$county_selector(),
-          cbsa = geo_filters$cbsa_selector(),
-          subsectors = input$subsector_select,
-          asset_sizes = input$size_filter,
-          time_series = time_series,
-          year_start = input$date_range[1],
-          year_end = input$date_range[2],
-          yearvar = groupby_var
+      if (input_validation_msg != TRUE) {
+        shiny::showModal(modal(modal_titles[["invalid_inputs"]], input_validation_msg))
+      }
+      else {
+        title <- create_plot_title(input$org_level, input$other_orgs, input$date_range, time_series, title_prefix)
+        subtitle <- create_plot_subtitle(
+          geo_filters$geo_level(),
+          geo_filters$region_selector(),
+          geo_filters$state_selector_single(),
+          geo_filters$state_selector_multi(),
+          geo_filters$county_selector(),
+          geo_filters$cbsa_selector(),
+          input$subsector_select,
+          input$size_filter
         )
-        setProgress(2, message = "Creating Tables...")
-        tables <- summarise_data(
-          data = filtered_data,
-          groupby_var = groupby_var,
-          sum_var = sum_var,
-          geo_level = geo_filters$geo_level(),
-          subsector_choices = input$subsector_select,
-          size_choices = input$size_filter
-        )
-        setProgress(3, message = "Creating Graphs...")
-        plots <- create_plots(
-          table_ls = tables,
-          single_plot_func = single_plot_func,
-          group_plot_func = group_plot_func,
-          geo_level = geo_filters$geo_level(),
-          subsector_choices = input$subsector_select,
-          size_choices = input$size_level,
-          title = title,
-          subtitle = subtitle,
-          yvar = sum_var,
-          xvar = groupby_var
-        )
-        setProgress(4, message = "Displaying Results...")
-        output$plot_overall <- renderPlot({
-          plots[["default"]]
-        })
-        output$table_overall <- renderReactable({
-          reactable(
-            tables[["default"]],
-            outlined = TRUE,
-            defaultPageSize = 10,
-            defaultColDef = colDef(align = "left")
+        shiny::withProgress(min = 1, max = 5, {
+          setProgress(1, message = "Filtering Data...")
+          filtered_data <- filter_data(
+            data = data,
+            org_level = input$org_level,
+            other_orgs = input$other_orgs,
+            geo_level = geo_filters$geo_level(),
+            region = geo_filters$region_selector(),
+            state_single = geo_filters$state_selector_single(),
+            state_mult = geo_filters$state_selector_multi(),
+            county = geo_filters$county_selector(),
+            cbsa = geo_filters$cbsa_selector(),
+            subsectors = input$subsector_select,
+            asset_sizes = input$size_filter,
+            time_series = time_series,
+            year_start = input$date_range[1],
+            year_end = input$date_range[2],
+            yearvar = groupby_var
           )
-        })
-        # Stage 5 Displaying Results - By Subsector
-        output$plot_subsector <- renderPlot({
-          plots[["by_subsector"]] + gghighlight::gghighlight(
-            `Subsector` %in% input$subsector_highlight,
+          setProgress(2, message = "Creating Tables...")
+          tables <- summarise_data(
+            data = filtered_data,
+            groupby_var = groupby_var,
+            sum_var = sum_var,
+            geo_level = geo_filters$geo_level(),
+            subsector_choices = input$subsector_select,
+            size_choices = input$size_filter
           )
-        })
-        output$table_subsector <- renderReactable({
-          if (length(input$subsector_select) > 0) {
-            shiny::req(tables[["by_subsector"]])
+          setProgress(3, message = "Creating Graphs...")
+          plots <- create_plots(
+            table_ls = tables,
+            single_plot_func = single_plot_func,
+            group_plot_func = group_plot_func,
+            geo_level = geo_filters$geo_level(),
+            subsector_choices = input$subsector_select,
+            size_choices = input$size_filter,
+            title = title,
+            subtitle = subtitle,
+            yvar = sum_var,
+            xvar = groupby_var
+          )
+          setProgress(4, message = "Displaying Results...")
+          output$plot_overall <- renderPlot({
+            plots[["default"]]
+          })
+          output$table_overall <- renderReactable({
             reactable(
-              tables[["by_subsector"]],
-              groupBy = "Subsector",
+              tables[["default"]],
               outlined = TRUE,
               defaultPageSize = 10,
-              defaultColDef = colDef(align = "center")
+              defaultColDef = colDef(align = "left")
             )
-          }
-        })
-        # Stage 5 Displaying Results - Geography
-        output$plot_geo <- renderPlot({
-          plots[["by_geo"]] + gghighlight::gghighlight(
-            `Census State` == input$individual_geo,
-          )
-        })
-        output$table_geo <- renderReactable({
-          if (geo_filters$geo_level() != "all") {
-            reactable(
-              tables[["by_geo"]],
-              groupBy = input$geo_level,
-              outlined = TRUE,
-              defaultPageSize = 10,
-              defaultColDef = colDef(align = "center")
+          })
+          # Stage 5 Displaying Results - By Subsector
+          output$plot_subsector <- renderPlot({
+            plots[["by_subsector"]] + gghighlight::gghighlight(
+              `Subsector` %in% input$subsector_highlight,
             )
-          }
-        })
-        output$plot_size <- renderPlot({
-          plots[["by_asset_size"]]
-        })
-        # Stage 5 Displaying Results - Asset Size
-        output$table_size <- renderReactable({
-          if (input$size_level == "individual") {
-            reactable(
-              tables[["by_asset_size"]],
-              groupBy = "Asset Size",
-              outlined = TRUE,
-              defaultPageSize = 10,
-              defaultColDef = colDef(align = "center")
+          })
+          output$table_subsector <- renderReactable({
+            if (length(input$subsector_select) > 0) {
+              shiny::req(tables[["by_subsector"]])
+              reactable(
+                tables[["by_subsector"]],
+                groupBy = "Subsector",
+                outlined = TRUE,
+                defaultPageSize = 10,
+                defaultColDef = colDef(align = "center")
+              )
+            }
+          })
+          # Stage 5 Displaying Results - Geography
+          output$plot_geo <- renderPlot({
+            plots[["by_geo"]] + gghighlight::gghighlight(
+              `Census State` == input$individual_geo,
             )
-          }
+          })
+          output$table_geo <- renderReactable({
+            if (geo_filters$geo_level() != "all") {
+              reactable(
+                tables[["by_geo"]],
+                groupBy = input$geo_level,
+                outlined = TRUE,
+                defaultPageSize = 10,
+                defaultColDef = colDef(align = "center")
+              )
+            }
+          })
+          output$plot_size <- renderPlot({
+            plots[["by_asset_size"]]
+          })
+          # Stage 5 Displaying Results - Asset Size
+          output$table_size <- renderReactable({
+            if (length(input$size_filter) > 0){
+              reactable(
+                tables[["by_asset_size"]],
+                groupBy = "Asset Size",
+                outlined = TRUE,
+                defaultPageSize = 10,
+                defaultColDef = colDef(align = "center")
+              )
+            }
+          })
+          setProgress(5, message = "Done!")
         })
-        setProgress(5, message = "Done!")
-      })
+      }
     })
   })
 }
