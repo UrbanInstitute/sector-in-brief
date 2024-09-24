42554a679e943af25885b6bee77d15261e781f77
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-24 19:48:25 -0400

    Reduce number of arguments

--- R/validate_inputs.R
+++ R/validate_inputs.R
@@ -1,46 +1,35 @@
-validate_inputs <- function(org_type,
-                            other_org_type,
-                            geo_level,
-                            geo_region,
-                            geo_state_multi,
-                            geo_state_single,
-                            geo_county,
-                            geo_cbsa,
-                            subsector,
-                            size,
-                            start_date,
-                            end_date){
+validate_inputs <- function(inputs){
   message <- TRUE
-  if (geo_level == "Census Region"){
-    if (length(geo_region) == 0){
+  if (inputs$geo_level == "Census Region"){
+    if (length(inputs$geo_region) == 0){
       message <- "Please select at least one region."
     }
   }
-  if (geo_level == "Census State"){
-    if (length(geo_state_multi) == 0){
+  if (inputs$geo_level == "Census State"){
+    if (length(inputs$geo_state_multi) == 0){
       message <- "Please select at least one state."
     }
   }
-  if (geo_level == "Census County"){
-    if (length(geo_state_single) == 0){
+  if (inputs$geo_level == "Census County"){
+    if (length(inputs$geo_state_single) == 0){
       message <- "Please select a state."
     }
-    if (length(geo_county) == 0){
+    if (length(inputs$geo_county) == 0){
       message <- "Please select at least one county."
     }
   }
-  if (geo_level == "Census CBSA"){
-    if (length(geo_state_single) == 0){
+  if (inputs$geo_level == "Census CBSA"){
+    if (length(inputs$geo_state_single) == 0){
       message <- "Please select a state."
     }
-    if (length(geo_cbsa) == 0){
+    if (length(inputs$geo_cbsa) == 0){
       message <- "Please select at least one CBSA."
     }
   }
-  if (length(subsector) == 0){
+  if (length(inputs$subsector) == 0){
     message <- "Please select at least one subsector."
   }
-  if (length(size) == 0){
+  if (length(inputs$size) == 0){
     message <- "Please select at least one asset size category."
   }
   return(message)
