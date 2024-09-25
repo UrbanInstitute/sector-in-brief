02c2f4a794a1499a7d58ba699944284e9a262d75
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-25 17:37:00 -0400

    Add missing arguments

--- R/group_col_plot.R
+++ R/group_col_plot.R
@@ -1,19 +1,15 @@
-group_col_plot <- function(table, grouping_var, title, subtitle, yvar, xvar, ytitle) {
+group_col_plot <- function(table, groupby_var, title, subtitle, yvar, xvar, ytitle, xtitle) {
   p <- ggplot(table, aes(
-    x = !!sym(grouping_var),
+    x = !!sym(groupby_var),
     y = !!sym(yvar),
-    fill = !!sym(grouping_var)
+    fill = !!sym(groupby_var)
   )) +
     geom_col(width = 0.9) +
     scale_fill_manual(values = colorpalette) +
-    scale_y_continuous(
-      limits = c(0, NA),
-      expand = expansion(mult = 0.1),
-      labels = scales::comma()
-    ) +
+    plot_scales +
     labs(
       subtitle = subtitle,
-      x = grouping_var,
+      x = groupby_var,
       title = title,
       y = ytitle
     ) +
