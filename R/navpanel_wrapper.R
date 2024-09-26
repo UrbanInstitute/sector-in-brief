2f03430aad0232654252785fa88ba655f3aaeb4d
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-25 21:17:43 -0400

    Change filter positions and keep correct filters for correct page

--- R/navpanel_wrapper.R
+++ R/navpanel_wrapper.R
@@ -1,9 +1,36 @@
 # Function to create nav panels
 navpanel_wrapper <- function(title, panel_header, panel_desc, panelid) {
+  all_cards <- data_ui(panelid, org_type_choices, date = TRUE)
   panel <- bslib::nav_panel(
     title = title,
     page_header_card(panel_header, panel_desc),
-    data_ui(panelid, org_type_choices, date = TRUE),
+    bslib::card(
+      card_header("Select Your Variables"),
+      title = "",
+      if (title == "Number" | title == "Finances"){
+        bslib::layout_column_wrap(
+          all_cards[["org_card"]],
+          all_cards[["date_card"]],
+          all_cards[["subsector_card"]],
+          all_cards[["size_card"]],
+          all_cards[["geo_card"]]
+        )
+      } else if (title == "PF_Grants"){
+        bslib::layout_column_wrap(
+          all_cards[["date_card"]],
+          all_cards[["subsector_card"]],
+          all_cards[["size_card"]],
+          all_cards[["geo_card"]],
+        )
+      } else if (title == "DAF"){
+        bslib::layout_column_wrap(
+          all_cards[["subsector_card"]],
+          all_cards[["size_card"]],
+          all_cards[["geo_card"]]
+        )
+      },
+      all_cards[["download_button"]]
+    ),
     plot_ui(panelid)
   )
   return(panel)
@@ -16,5 +43,5 @@ navpanels <- tibble::tribble(
   "PF_Grants", "Private Foundation Grants", "XXX", "pf_grants",
   "DAF", "Donor Advised Funds (DAFs)", "XXX", "daf",
 )
-
-navpanels <- purrr::pmap(navpanels, navpanel_wrapper)
+                 
+  
