f0d884ddd4c4167aa70f0ab7e8b47948ccd9b6a8
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-27 02:36:17 -0400

    Place buttons side-by-side

--- R/navpanel_wrapper.R
+++ R/navpanel_wrapper.R
@@ -29,8 +29,11 @@ navpanel_wrapper <- function(title, panel_header, panel_desc, panelid) {
           all_cards[["geo_card"]]
         )
       },
-      all_cards[["process_button"]],
-      all_cards[["clear_button"]]
+      bslib::layout_column_wrap(
+        width = 1/2,
+        all_cards[["process_button"]],
+        all_cards[["clear_button"]]
+      )
     ),
     plot_ui(panelid)
   )
