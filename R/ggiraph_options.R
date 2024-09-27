f04adf0552f7c5b6b0662cf5147eed386f7245b6
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-27 14:31:05 -0400

    Options for interactive plots

---
+++ R/ggiraph_options.R
@@ -0,0 +1,6 @@
+ggiraph_options <- list(
+  ggiraph::opts_hover(css = "stroke-width: 3px; transition: all 0.3s ease;"),
+  ggiraph::opts_hover_inv(css = "opacity:0.1;"),
+  ggiraph::opts_sizing(rescale = TRUE),
+  width = 1
+)
