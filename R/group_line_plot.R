e3868d0511e73a29fbf13f83f3db4331379207cd
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-27 14:28:30 -0400

    Refactor plotting components into separate scripts

--- R/group_line_plot.R
+++ R/group_line_plot.R
@@ -7,31 +7,23 @@ group_line_plot <- function(table,
                             ytitle,
                             xtitle,
                             num_groups) {
-  p <- ggplot(table, aes(
-    x = !!sym(xvar),
-    y = !!sym(yvar),
-    colour = !!sym(groupby_var),
-    data_id = !!sym(groupby_var)
-  )) +
+  p <- ggplot(table,
+              aes(
+                x = !!sym(xvar),
+                y = !!sym(yvar),
+                colour = !!sym(groupby_var),
+                data_id = !!sym(groupby_var)
+              )) +
     ggiraph::geom_line_interactive(size = 1.5, hover_nearest = FALSE) +
     ggiraph::geom_point_interactive(
-      aes(tooltip = paste(
-        "<b>",
-        groupby_var,
-        "</b>: ",
-        !!sym(groupby_var),
-        "<br><b>",
-        yvar,
-        "</b>: ",
-        !!sym(yvar)
-      )),
+      aes(tooltip = tooltip_text(table, groupby_var, yvar, xvar)),
       size = 3,
       fill = "white",
       shape = 21,
       stroke = 1.2,
       hover_nearest = TRUE
     ) +
-    scale_color_manual(values = colorpalette(num_colors=num_groups)) +
+    scale_color_manual(values = colorpalette(num_colors = num_groups)) +
     plot_scales +
     labs(
       subtitle = subtitle,
@@ -39,12 +31,9 @@ group_line_plot <- function(table,
       title = title,
       y = ytitle
     ) +
-    scale_x_continuous(breaks = seq(1990, 2024, 4)) +
     plot_theme
-  p <- ggiraph::girafe(
-    ggobj = p,
-    width_svg = 15,
-    options = list(opts_sizing(rescale = TRUE), width = 1)
-  )
+  p <- ggiraph::girafe(ggobj = p,
+                       width_svg = 20,
+                       options = ggiraph_options)
   return(p)
 }
