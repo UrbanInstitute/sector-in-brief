06b5b7279fa103f696af8b5259e90a4b4ab63cfe
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-27 00:12:29 -0400

    Set default to NULL

--- R/dataloader.R
+++ R/dataloader.R
@@ -1,5 +1,5 @@
 # Function to load data based on input tab
-dataloader <- function(path, cols) {
+dataloader <- function(path, cols=NULL) {
   shinycssloaders::showPageSpinner()
   data_select <- arrow::read_parquet(path, as_data_frame = FALSE, col_select = cols)
   shinycssloaders::hidePageSpinner()
