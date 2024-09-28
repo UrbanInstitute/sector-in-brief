a57ba705279abb2a991dd7aad95db1fcf66bfa85
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-27 20:14:03 -0400

    Update summarise for different metrics

--- R/summarise_data.R
+++ R/summarise_data.R
@@ -3,35 +3,78 @@ summarise_data <- function(data, groupby_var, sum_var, query) {
   # Params
   geo_level <- query$geo_level
   # Create tables
-  table_default <- data |>
-    group_by(!!sym(groupby_var)) |>
-    summarise(!!sum_var := sum(!!sym(sum_var), na.rm = TRUE)) |>
-    dplyr::collect()
-  table_ls <- list("default" = table_default)
-  table_by_geo <- data |>
-    dplyr::group_by(!!sym(groupby_var), !!sym(geo_level)) |>
-    summarise(!!sum_var := sum(!!sym(sum_var), na.rm = TRUE)) |>
-    dplyr::collect()
-  table_ls[["by_geo"]] <- table_by_geo
-  table_by_subsector <- data |>
-    group_by(!!sym(groupby_var), Subsector) |>
-    summarise(!!sum_var := sum(!!sym(sum_var), na.rm = TRUE)) |>
-    dplyr::collect()
-  table_ls[["by_subsector"]] <- table_by_subsector
-  table_by_asset_size <- data |>
-    dplyr::group_by(!!sym(groupby_var), `Asset Size`) |>
-    dplyr::summarise(!!sum_var := sum(!!sym(sum_var), na.rm = TRUE)) |>
-    dplyr::mutate(
-      "Asset Size" = case_when(
-        `Asset Size` <= 1 ~ "Under $100,000",
-        `Asset Size` == 2 ~ "$100,000 - $499,999",
-        `Asset Size` == 3 ~ "$500,000 - $999,999",
-        `Asset Size` == 4 ~ "$1 Million - $4.99 Million",
-        `Asset Size` == 5 ~ "$5 Million - $9.99 Million",
-        `Asset Size` == 6 ~ "Above $10 Million",
-      )
-    ) |>
-    dplyr::collect()
-  table_ls[["by_asset_size"]] <- table_by_asset_size
-  return(table_ls)
+  if (sum_var == "Proportion with DAFs") {
+    table_default <- data |>
+      group_by(!!sym(groupby_var)) |>
+      dplyr::summarise(!!sum_var := round(
+        sum(`Has DAF`, na.rm = TRUE) / sum(`Number of Nonprofits`, na.rm = TRUE),
+        2
+      ) * 100) |>
+      dplyr::collect()
+    table_ls <- list("default" = table_default)
+    table_by_geo <- data |>
+      dplyr::group_by(!!sym(groupby_var), !!sym(geo_level)) |>
+      dplyr::summarise(!!sum_var := round(
+        sum(`Has DAF`, na.rm = TRUE) / sum(`Number of Nonprofits`, na.rm = TRUE),
+        2
+      ) * 100) |>      dplyr::collect()
+    table_ls[["by_geo"]] <- table_by_geo
+    table_by_subsector <- data |>
+      group_by(!!sym(groupby_var), Subsector) |>
+      dplyr::summarise(!!sum_var := round(
+        sum(`Has DAF`, na.rm = TRUE) / sum(`Number of Nonprofits`, na.rm = TRUE),
+        2
+      ) * 100) |>      dplyr::collect()
+    table_ls[["by_subsector"]] <- table_by_subsector
+    table_by_asset_size <- data |>
+      dplyr::group_by(!!sym(groupby_var), `Asset Size`) |>
+      dplyr::summarise(!!sum_var := round(
+        sum(`Has DAF`, na.rm = TRUE) / sum(`Number of Nonprofits`, na.rm = TRUE),
+        2
+      ) * 100) |>      dplyr::mutate(
+        "Asset Size" = case_when(
+          `Asset Size` <= 1 ~ "Under $100,000",
+          `Asset Size` == 2 ~ "$100,000 - $499,999",
+          `Asset Size` == 3 ~ "$500,000 - $999,999",
+          `Asset Size` == 4 ~ "$1 Million - $4.99 Million",
+          `Asset Size` == 5 ~ "$5 Million - $9.99 Million",
+          `Asset Size` == 6 ~ "Above $10 Million",
+        )
+      ) |>
+      dplyr::collect()
+    table_ls[["by_asset_size"]] <- table_by_asset_size
+    return(table_ls)
+  } else {
+    table_default <- data |>
+      group_by(!!sym(groupby_var)) |>
+      summarise(!!sum_var := sum(!!sym(sum_var), na.rm = TRUE)) |>
+      dplyr::collect()
+    table_ls <- list("default" = table_default)
+    table_by_geo <- data |>
+      dplyr::group_by(!!sym(groupby_var), !!sym(geo_level)) |>
+      summarise(!!sum_var := sum(!!sym(sum_var), na.rm = TRUE)) |>
+      dplyr::collect()
+    table_ls[["by_geo"]] <- table_by_geo
+    table_by_subsector <- data |>
+      group_by(!!sym(groupby_var), Subsector) |>
+      summarise(!!sum_var := sum(!!sym(sum_var), na.rm = TRUE)) |>
+      dplyr::collect()
+    table_ls[["by_subsector"]] <- table_by_subsector
+    table_by_asset_size <- data |>
+      dplyr::group_by(!!sym(groupby_var), `Asset Size`) |>
+      dplyr::summarise(!!sum_var := sum(!!sym(sum_var), na.rm = TRUE)) |>
+      dplyr::mutate(
+        "Asset Size" = case_when(
+          `Asset Size` <= 1 ~ "Under $100,000",
+          `Asset Size` == 2 ~ "$100,000 - $499,999",
+          `Asset Size` == 3 ~ "$500,000 - $999,999",
+          `Asset Size` == 4 ~ "$1 Million - $4.99 Million",
+          `Asset Size` == 5 ~ "$5 Million - $9.99 Million",
+          `Asset Size` == 6 ~ "Above $10 Million",
+        )
+      ) |>
+      dplyr::collect()
+    table_ls[["by_asset_size"]] <- table_by_asset_size
+    return(table_ls)
+  }
 }
