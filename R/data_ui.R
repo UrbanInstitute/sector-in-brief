7bbf4aa0fb18be5d9de046ad4ab89edf321b8b0f
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-27 20:47:15 -0400

    Remove margin argument and extend width to keep fillability and center slider

--- R/data_ui.R
+++ R/data_ui.R
@@ -2,7 +2,8 @@
 data_ui <- function(id, org_type_choices, date) {
   tagList(
     org_card = bslib::card(
-      bslib::card_header("Organization Type"),
+      bslib::card_header("Organization Type", 
+                         shiny::actionLink(shiny::NS(id, "org_reset"), "Reset", style = "float: right;")),
       shiny::selectizeInput(
         inputId = shiny::NS(id, "org_level"),
         label = NULL,
@@ -26,43 +27,49 @@ data_ui <- function(id, org_type_choices, date) {
         ns = shiny::NS(id)
       )
     ),
-    geo_card = bslib::card(
-      card_header("Geography"),
-      geo_filter_ui(NS(id, "geo_filter"), state_choices),
-      
-    ),
+    geo_card = geo_filter_ui(NS(id, "geo_filter"), state_choices),
     subsector_card = bslib::card(
-      bslib::card_header("Subsector"),
-      shiny::checkboxGroupInput(
-        inputId = shiny::NS(id, "subsector_select"),
-        label = NULL,
-        choices = subsector_choices,
-        selected = subsector_choices,
-        inline = FALSE
+      bslib::card_header("Subsector", shiny::actionLink(shiny::NS(id, "subsector_reset"), "Reset", style = "float: right;")),
+      htmltools::div(
+        class = "filter-options",
+        shiny::checkboxGroupInput(
+          inputId = shiny::NS(id, "subsector_select"),
+          label = NULL,
+          choices = subsector_choices,
+          selected = subsector_choices,
+          inline = FALSE
+        )
       )
     ),
     size_card = bslib::card(
-      card_header("Asset Size"),
-      shiny::checkboxGroupInput(
-        inputId = shiny::NS(id, "size_filter"),
-        label = NULL,
-        inline = FALSE,
-        choices = size_choices,
-        selected = size_choices
+      card_header("Asset Size", shiny::actionLink(shiny::NS(id, "size_reset"), "Reset", style = "float: right;")),
+      htmltools::div(
+        class = "filter-options",
+        shiny::checkboxGroupInput(
+          inputId = shiny::NS(id, "size_filter"),
+          label = NULL,
+          inline = FALSE,
+          choices = size_choices,
+          selected = size_choices
+        )
       )
     ),
     date_card = bslib::card(
-      card_header("Date Range"),
-      shiny::sliderInput(
-        inputId = shiny::NS(id, "date_range"),
-        label = NULL,
-        min = 1989,
-        max = 2024,
-        value = c(1989, 2024),
-        step = NULL,
-        ticks = FALSE,
-        sep = "",
-        dragRange = TRUE
+      card_header("Date Range", shiny::actionLink(shiny::NS(id, "date_reset"), "Reset", style = "float: right;")),
+      htmltools::div(
+       class = "slider",
+       shiny::sliderInput(
+         inputId = shiny::NS(id, "date_range"),
+         label = NULL,
+         min = 1989,
+         max = 2024,
+         value = c(1989, 2024),
+         step = NULL,
+         ticks = FALSE,
+         sep = "",
+         dragRange = TRUE,
+         width = "100%"
+       )
       )
     ),
     process_button = bslib::input_task_button(
@@ -70,13 +77,6 @@ data_ui <- function(id, org_type_choices, date) {
       style = "border-radius: 0; font-size: 18px; color: #ffffff; margin: auto; background-color: #1696d2; border-color: #1696d2;",
       label = "RETRIEVE DATA",
       label_busy = "UPDATING PLOTS",
-      type = "primary"
-    ),
-    clear_button = bslib::input_task_button(
-      id = shiny::NS(id, "clear_filters"),
-      style = "border-radius: 0; font-size: 18px; color: #ffffff; margin: auto; background-color: #1696d2; border-color: #1696d2;",
-      label = "CLEAR FILTERS",
-      label_busy = "CLEARING FILTERS",
       type = "primary"
     )
   )
