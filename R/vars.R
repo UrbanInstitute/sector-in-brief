f8302b9d69d3da13057f6d948632688ad3b653e3
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-27 20:26:04 -0400

    Add variables to load for DAFs and PFs

--- R/vars.R
+++ R/vars.R
@@ -10,11 +10,23 @@ default_vars <- c(
 )
 
 
+daf_vars <- c(
+  "Number of Nonprofits",
+  "Has DAF",
+  "Number of DAFs",
+  "Total Contributions",
+  "Total Grants",
+  "Total Value",
+  "Proportion With DAFs"
+)
+
 var_ls <- list(
   "Number" = c(default_vars, "Number of Nonprofits", "Year"),
   "Assets" = c(default_vars, "Total Assets", "Tax Year"),
   "Revenues" = c(default_vars, "Total Revenues", "Tax Year"),
   "Expenses" = c(default_vars, "Total Expenses", "Tax Year"),
   "Benefits" = c(default_vars, "Total Benefits", "Tax Year"),
-  "Payroll Taxes" = c(default_vars, "Total Payroll Taxes", "Tax Year")
+  "Payroll Taxes" = c(default_vars, "Total Payroll Taxes", "Tax Year"),
+  "Private Foundations" = c(default_vars, "Total Contributions", "Tax Year"),
+  "Donor Advised Funds" = c(default_vars, daf_vars)
 )
