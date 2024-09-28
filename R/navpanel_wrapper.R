c93ef87d51e8d2757d63f21df3f2bd9745b4137e
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-27 20:16:03 -0400

    Debug missing filters for Private Foundations

--- R/navpanel_wrapper.R
+++ R/navpanel_wrapper.R
@@ -9,7 +9,7 @@ navpanel_wrapper <- function(title, panel_header, panel_desc, panelid) {
       title = "",
       if (title %in% c("Number", "Assets", "Revenues", "Expenses", "Benefits", "Payroll Taxes")) {
         bslib::layout_column_wrap(all_cards[["org_card"]], all_cards[["date_card"]], all_cards[["subsector_card"]], all_cards[["size_card"]], all_cards[["geo_card"]])
-      } else if (grepl("PF", title)) {
+      } else if (title %in% c("Total Grant Amount")) {
         bslib::layout_column_wrap(all_cards[["date_card"]], all_cards[["subsector_card"]], all_cards[["size_card"]], all_cards[["geo_card"]], )
       } else if (grepl("DAF", title)) {
         bslib::layout_column_wrap(all_cards[["subsector_card"]], all_cards[["size_card"]], all_cards[["geo_card"]])
@@ -29,8 +29,6 @@ navpanels <- tibble::tribble(
   "Expenses", "Total Expenses", "The total amount of money that nonprofits spend", "expenses",
   "Benefits", "Total Benefits", "The aggregate value of salaries, wages, benefits, and pension plan contributions nonprofits and private foundations pay to/on behalf of employees.", "benefits",
   "Payroll Taxes", "Total Payroll Taxes", "The estimated aggregate value of the taxes nonprofits and private foundations pay on employee earnings.", "payroll",
-  "Number of Grants", "Total Number of Grants", "Private foundations are charitable organizations that typically receive most of their funding from a single source and primarily exist to make grants, rather than operate programs.", "pf_number",
-  "Median Grant Amount", "Median Grant Size", "Private foundations are charitable organizations that typically receive most of their funding from a single source and primarily exist to make grants, rather than operate programs.", "pf_median",
   "Total Grant Amount", "Total Amount of Grants Paid", "Private foundations are charitable organizations that typically receive most of their funding from a single source and primarily exist to make grants, rather than operate programs.", "pf_amount",
   "Number of DAFs", "Total Number of Donor Advised Funds", "Donor-advised funds (DAFs) are charitable giving accounts that allow donors to receive an immediate tax benefit and recommend grants from the fund over time.", "daf_number",
   "DAF Contributions", "Total Contributions to Donor Advised Funds", "Donor-advised funds (DAFs) are charitable giving accounts that allow donors to receive an immediate tax benefit and recommend grants from the fund over time.", "daf_contributions",
