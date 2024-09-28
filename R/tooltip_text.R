54802dff22bad39074563ba680b5e1ab10036313
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-28 17:16:02 -0400

    Add 2 tooltip options, for grouped and single plots

--- R/tooltip_text.R
+++ R/tooltip_text.R
@@ -1,17 +1,30 @@
-tooltip_text <- function(table, groupby_var, yvar, xvar) {
-  text <- paste(
-    "<b>",
-    groupby_var,
-    "</b>: ",
-    table[[groupby_var]],
-    "<br><b>",
-    yvar,
-    "</b>: ",
-    scales::comma(table[[yvar]]),
-    "<br><b>",
-    xvar,
-    "</b>: ",
-    table[[xvar]]
-  )
+tooltip_text <- function(table, yvar, xvar, groupby_var=NULL) {
+  if (is.null(groupby_var)) {
+    text <- paste(
+      "<b>",
+      yvar,
+      "</b>: ",
+      scales::comma(table[[yvar]]),
+      "<br><b>",
+      xvar,
+      "</b>: ",
+      table[[xvar]]
+    )
+  } else {
+    text <- paste(
+      "<b>",
+      groupby_var,
+      "</b>: ",
+      table[[groupby_var]],
+      "<br><b>",
+      yvar,
+      "</b>: ",
+      scales::comma(table[[yvar]]),
+      "<br><b>",
+      xvar,
+      "</b>: ",
+      table[[xvar]]
+    ) 
+  }
   return(text)
 }
