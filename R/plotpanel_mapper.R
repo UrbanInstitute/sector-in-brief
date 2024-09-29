dc1bce99e3f3c0baee7391bc5f5767d0d03f2204
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-28 21:28:48 -0400

    Debug incorrect function name

--- R/plotpanel_mapper.R
+++ R/plotpanel_mapper.R
@@ -1,5 +1,5 @@
 plotpanel_mapper <- function(plotpanel_args, id){
-  plotpanels <- purrr::pmapper(plotpanel_args, plotpanel_builder, id = id)
+  plotpanels <- purrr::pmap(plotpanel_args, plotpanel_builder, id = id)
   names(plotpanels) <- plotpanel_args[["title"]]
   return(plotpanels)
 }
