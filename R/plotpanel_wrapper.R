e8f498e6fe4d6ead0ec52530e61735dfa1e32123
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-28 21:04:32 -0400

    Wrapper functon for plot panels

---
+++ R/plotpanel_wrapper.R
@@ -0,0 +1,25 @@
+plotpanel_wrapper <- function(id, title, plot_id, table_id, download_id){
+  bslib::nav_panel(
+    title = title,
+    layout_column_wrap(
+      width = NULL,
+      heigh = 650,
+      style = htmltools::css(grid_template_columns = "3fr 1fr"),
+      bslib::card(
+        bslib::card_body(
+          shinycssloaders::withSpinner(
+            ggiraph::girafeOutput(NS(id, plot_id), width = "100%"),
+            type = 1
+          )
+        ),
+        plot_footer
+      ),
+      bslib::card(
+        bslib::card_body(reactable::reactableOutput(NS(id, table_id))),
+        bslib::card_body(
+          downloadButton(NS(id, download_id), "DOWNLOAD", class = "btn-download", icon = NULL)
+        )
+      )
+    )
+  )
+}
