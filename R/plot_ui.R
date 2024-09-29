68652693ff2271892f5c21d75d9a21b8f52bdbd5
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-28 22:51:05 -0400

    Refactor plot panels

--- R/plot_ui.R
+++ R/plot_ui.R
@@ -1,99 +1,12 @@
 plot_ui <- function(id, selected_geographies) {
+  plotpanels <- plotpanel_mapper(plotpanel_args, id)
   bslib::navset_card_tab(
     title =   "",
     height = "100%",
     bslib::card_title("Visualize Your Results", class = "viz-header"),
-    bslib::nav_panel(
-      "Overall",
-      layout_column_wrap(
-        width = NULL,
-        heigh = 650,
-        style = htmltools::css(grid_template_columns = "3fr 1fr"),
-        bslib::card(
-          bslib::card_body(
-            shinycssloaders::withSpinner(
-              ggiraph::girafeOutput(NS(id, "plot_overall"), width = "100%"),
-              type = 1
-            )
-          ),
-          plot_footer
-        ),
-        bslib::card(
-          bslib::card_body(reactable::reactableOutput(NS(id, "table_overall"))),
-          bslib::card_body(
-            downloadButton(NS(id, "downloadData"), "DOWNLOAD", class = "btn-download", icon = NULL)
-          )
-        )
-      )
-    ),
-    bslib::nav_panel(
-      "By Subsector",
-      layout_column_wrap(
-        width = NULL,
-        height = 650,
-        style = htmltools::css(grid_template_columns = "3fr 1fr"),
-        bslib::card(
-          bslib::card_body(
-            shinycssloaders::withSpinner(
-              ggiraph::girafeOutput(NS(id, "plot_subsector"), width = "100%"),
-              type = 1
-            )
-          ),
-          plot_footer
-        ),
-        bslib::card(
-          bslib::card_body(reactable::reactableOutput(NS(id, "table_subsector"))),
-          bslib::card_body(
-            downloadButton(NS(id, "downloadData"), "DOWNLOAD", class = "btn-download", icon = NULL)
-          )
-        )
-      )
-    ),
-    bslib::nav_panel(
-      "By Geography",
-      layout_column_wrap(
-        width = NULL,
-        height = 650,
-        style = htmltools::css(grid_template_columns = "3fr 1fr"),
-        bslib::card(
-          bslib::card_body(
-            shinycssloaders::withSpinner(
-              ggiraph::girafeOutput(NS(id, "plot_geo"), width = "100%"),
-              type = 1
-            )
-          ),
-          plot_footer
-        ),
-        bslib::card(
-          bslib::card_body(reactable::reactableOutput(NS(id, "table_geo"))),
-          bslib::card_body(
-            downloadButton(NS(id, "downloadData"), "DOWNLOAD", class = "btn-download", icon = NULL)
-          )
-        )
-      )
-    ),
-    bslib::nav_panel(
-      "By Asset Size",
-      layout_column_wrap(
-        width = NULL,
-        height = 650,
-        style = htmltools::css(grid_template_columns = "3fr 1fr"),
-        bslib::card(
-          bslib::card_body(
-            shinycssloaders::withSpinner(
-              ggiraph::girafeOutput(NS(id, "plot_size"), width = "100%"),
-              type = 1
-            )
-          ),
-          plot_footer
-        ),
-        bslib::card(
-          bslib::card_body(reactable::reactableOutput(NS(id, "table_size"))),
-          bslib::card_body(
-            downloadButton(NS(id, "downloadData"), "DOWNLOAD", class = "btn-download", icon = NULL)
-          )
-        )
-      )
-    )
+    plotpanels[["Overall"]],
+    plotpanels[["By Subsector"]],
+    plotpanels[["By Geography"]],
+    plotpanels[["By Asset Size"]]
   )
 }
