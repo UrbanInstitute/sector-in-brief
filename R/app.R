15aa33173f0c5e746c2b8ef4a16c3f684b43c202
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-28 22:48:17 -0400

    Refactor data_server function calls

--- R/app.R
+++ R/app.R
@@ -24,10 +24,7 @@ app <- function(...) {
           visualpanels[["Benefits"]],
           visualpanels[["Payroll Taxes"]]        )
       ),
-      bslib::nav_panel(
-        title = "Private Foundations",
-        visualpanels[["Total Grant Amount"]]
-      ),
+      visualpanels[["Total Grant Amount"]],
       bslib::nav_panel(
         title = "Donor Advised Funds",
         bslib::navset_card_pill(
@@ -48,163 +45,16 @@ app <- function(...) {
     # Data Wrangling
     daf_title_prefix <- "Donor Advised Funds For: "
     data_select <- observeEvent( input$tabs, {
-      if(input$tabs == "Number"){
-        data <- dataloader("data/number_nonprofits.parquet",
-                           cols = var_ls[[input$tabs]])
-        data_server(
-          id = "number",
-          geo_df = geo_df,
-          data = data,
-          groupby_var = "Year",
-          sum_var = "Number of Nonprofits",
-          ytitle = "Number of Nonprofits",
-          xtitle = "Year",
-          title_prefix = "Number of"
-        )
-      }
-      else if (input$tabs == "Finances"){
+      if (input$tabs == "Finances"){
         observeEvent(input$finances, {
-          if (input$finances == "Assets"){
-            data <- dataloader("data/finances.parquet",
-                               cols = var_ls[[input$finances]])
-            data_server(
-              id = "assets",
-              geo_df = geo_df,
-              data = data,
-              groupby_var = "Tax Year",
-              sum_var = "Total Assets",
-              ytitle = "Dollars",
-              xtitle = "Tax Year",
-              title_prefix = "Total Assets For"
-            )
-          } else if (input$finances == "Revenues"){
-            data <- dataloader("data/finances.parquet",
-                               cols = var_ls[[input$finances]])
-            data_server(
-              id = "revenues",
-              geo_df = geo_df,
-              data = data,
-              groupby_var = "Tax Year",
-              sum_var = "Total Revenues",
-              ytitle = "Dollars",
-              xtitle = "Tax Year",
-              title_prefix = "Total Revenues For"
-            )
-          } else if (input$finances == "Expenses"){
-            data <- dataloader("data/finances.parquet",
-                               cols = var_ls[[input$finances]])
-            data_server(
-              id = "expenses",
-              geo_df = geo_df,
-              data = data,
-              groupby_var = "Tax Year",
-              sum_var = "Total Expenses",
-              ytitle = "Dollars",
-              xtitle = "Tax Year",
-              title_prefix = "Total Expenses For"
-            )
-          } else if (input$finances == "Benefits"){
-            data <- dataloader("data/finances.parquet",
-                               cols = var_ls[[input$finances]])
-            data_server(
-              id = "benefits",
-              geo_df = geo_df,
-              data = data,
-              groupby_var = "Tax Year",
-              sum_var = "Total Benefits",
-              ytitle = "Dollars",
-              xtitle = "Tax Year",
-              title_prefix = "Total Benefits For"
-            )
-          } else if (input$finances == "Payroll Taxes"){
-            data <- dataloader("data/finances.parquet",
-                               cols = var_ls[[input$finances]])
-            data_server(
-              id = "payroll_taxes",
-              geo_df = geo_df,
-              data = data,
-              groupby_var = "Tax Year",
-              sum_var = "Total Payroll Taxes",
-              ytitle = "Dollars",
-              xtitle = "Tax Year",
-              title_prefix = "Total Payroll Taxes For"
-            )
-          }
+          data_server_wrapper(input$finances, data_server_args, geo_df)
         })
-      } else if (input$tabs == "Private Foundations"){
-        data <- dataloader("data/pf_grants.parquet",
-                           cols = var_ls[[input$tabs]])
-        data_server(
-          id = "pf_amount",
-          geo_df = geo_df,
-          data = data,
-          groupby_var = "Tax Year",
-          sum_var = "Total Contributions",
-          ytitle = "Total Number of Grants",
-          xtitle = "Tax Year",
-          title_prefix = "Total Private Foundation Grant Contributions For"
-        )
-      }
-      else if(input$tabs == "Donor Advised Funds"){
-        data <- dataloader("data/daf.parquet",
-                           cols = var_ls[[input$tabs]])
-        data <- data |> dplyr::mutate(`Tax Year` = 2021)
-        print(head(data))
-        data_server(
-          id = "daf_number",
-          geo_df = geo_df,
-          data = data,
-          groupby_var = "Tax Year",
-          sum_var = "Number of DAFs",
-          ytitle = "Number of Donor Advised Funds",
-          xtitle = "Tax Year",
-          title_prefix = "Number of Donor Advised Funds For",
-          time_series = FALSE
-        )
-        data_server(
-          id = "daf_contributions",
-          geo_df = geo_df,
-          data = data,
-          groupby_var = "Tax Year",
-          sum_var = "Total Contributions",
-          ytitle = "Dollars",
-          xtitle = "Tax Year",
-          title_prefix = "Total Contributions to Donor Advised Funds For",
-          time_series = FALSE
-        )
-        data_server(
-          id = "daf_grants",
-          geo_df = geo_df,
-          data = data,
-          groupby_var = "Tax Year",
-          sum_var = "Total Grants",
-          ytitle = "Dollars",
-          xtitle = "Tax Year",
-          title_prefix = "Total Grants from Donor Advised Funds For",
-          time_series = FALSE
-        )
-        data_server(
-          id = "daf_value",
-          geo_df = geo_df,
-          data = data,
-          groupby_var = "Tax Year",
-          sum_var = "Total Value",
-          ytitle = "Dollars",
-          xtitle = "Tax Year",
-          title_prefix = "Total Value of Donor Advised Funds For",
-          time_series = FALSE
-        )
-        data_server(
-          id = "daf_proportion",
-          geo_df = geo_df,
-          data = data,
-          groupby_var = "Tax Year",
-          sum_var = "Proportion With DAFs",
-          ytitle = "Percentage",
-          xtitle = "Tax Year",
-          title_prefix = "Percentage of Nonprofits that maintain a DAF For",
-          time_series = FALSE
-        )
+      } else if (input$tabs == "Donor Advised Funds"){
+        observeEvent(input$daf, {
+          data_server_wrapper(input$daf, data_server_args, geo_df)
+        })
+      } else if (input$tabs %in% c("Number", "Total Grant Amount")){
+        data_server_wrapper(input$tabs, data_server_args, geo_df)
       }
     })
 
