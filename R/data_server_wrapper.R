37f0cb44b4c9da8807447d18e77ddcdf75ddc747
Thiyaghessan <thiyaghessan@gmail.com>
2024-09-28 22:20:27 -0400

    Wrapper function for loading data and creating server

---
+++ R/data_server_wrapper.R
@@ -0,0 +1,15 @@
+# Wrapper function for data_server() and dataloader()
+data_server_wrapper <- function(page, data_server_args, geo_df){
+  data <- dataloader(path = data_server_args[[path]][[path]], 
+                     vars = data_server_args[[path]][[vars]])
+  data_server(
+    id = data_server_args[[page]][[id]],
+    data = data,
+    geo_df = geo_df,
+    year_var = data_server_args[[page]][[year_var]],
+    agg_var = data_server_args[[page]][[agg_var]],
+    ytitle = data_server_args[[page]][[ytitle]],
+    xtitle = data_server_args[[page]][[xtitle]],
+    title_prefix = data_server_args[[page]][[title_prefix]]
+  )
+}
