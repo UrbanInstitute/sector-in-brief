afe174a9b2e885230ed80381c7223cff4c855452
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-02 20:28:43 -0400

    Add click effects

--- R/ggiraph_options.R
+++ R/ggiraph_options.R
@@ -1,6 +1,7 @@
 ggiraph_options <- list(
-  ggiraph::opts_hover(css = "stroke-width: 3px; transition: all 0.3s ease;"),
-  ggiraph::opts_hover_inv(css = "opacity:0.1;"),
+  ggiraph::opts_hover(css = "stroke-width: 3px; transition: all 0.3s ease; r: 6; fill-opacity: 1"),
+  ggiraph::opts_hover_inv(css = "opacity: 0.1;"),
+  ggiraph::opts_selection(css = "r: 6; stroke-width: 3px;"),
   ggiraph::opts_sizing(rescale = TRUE),
   width = 1
 )
