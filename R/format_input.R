4b662eaaa802ef0e263f47720c33682627d11de5
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-08 19:56:31 -0400

    Add function documentation and add arguments for caption formatting

--- R/format_input.R
+++ R/format_input.R
@@ -1,4 +1,17 @@
-format_input <- function(input, geo_filters, time_series, title_prefix, year_var){
+#' @title Format inputs for data pipeline
+#' @param input A list of reactive user inputs
+#' @param geo_filters A list of reactive geographic filters
+#' @param time_series A boolean indicating time series data
+#' @param title_prefix A character vector to prefix plot titles
+#' @param year_var A character vector defining the year column
+#' @param agg_var A character vector defining the column to aggregate
+#' @return A list of formatted inputs
+format_input <- function(input,
+                         geo_filters,
+                         time_series,
+                         title_prefix,
+                         year_var,
+                         agg_var) {
   inputs <- list(
     org_level = input$org_level,
     other_orgs = input$other_orgs,
@@ -13,7 +26,8 @@ format_input <- function(input, geo_filters, time_series, title_prefix, year_var
     year_range = input$date_range,
     time_series = time_series,
     title_prefix = title_prefix,
-    year_var = year_var
+    year_var = year_var,
+    agg_var = agg_var
   )
   return(inputs)
 }
