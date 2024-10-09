2baf2f5f7f8193db85b11f1f3fe4e0bdc56cbea1
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-08 22:06:18 -0400

    New function to plot subtitle

---
+++ R/plot_subtitle.R
@@ -0,0 +1,11 @@
+#' @title Function to create a subtitle for plot based on groupby variable
+#' @param groupby_var A character string that is the name of the variable to group by
+#' @return A character string that is the subtitle for the plot
+plot_subtitle <- function(groupby_var){
+  if (is.null) {
+    subtitle <- NULL
+  } else {
+    subtitle <- paste("By ", groupby_var) 
+  }
+  return(subtitle)
+}
