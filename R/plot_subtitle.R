2462321a4f0b5354d008aab120e999dcbc858bd2
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-10 15:09:03 -0400

    Debug missing null argument

--- R/plot_subtitle.R
+++ R/plot_subtitle.R
@@ -2,8 +2,8 @@
 #' @param groupby_var A character string that is the name of the variable to group by
 #' @return A character string that is the subtitle for the plot
 plot_subtitle <- function(groupby_var){
-  if (is.null) {
-    subtitle <- NULL
+  if (is.null(groupby_var)) {
+    subtitle <- ""
   } else {
     subtitle <- paste("By ", groupby_var) 
   }
