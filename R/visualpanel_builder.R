b5e99764c1ef1f85448b6bcc1b59cd3af29eea0d
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-10 15:12:48 -0400

    Add function documentation, populate choices based on panelid

--- R/visualpanel_builder.R
+++ R/visualpanel_builder.R
@@ -1,13 +1,30 @@
-# Function to create nav panel template for each visual page
-visualpanel_builder <- function(title, panel_header, panel_desc, panelid, start_year, end_year) {
-  all_cards <- data_ui(panelid, org_type_choices, start_year, end_year)
+#' @title Function to create nav panel template for each visual page
+#' @param title The title of the visual page
+#' @param panel_header The header of the visual page
+#' @param panel_desc The description of the visual page
+#' @param panelid The id of the visual page
+#' @param start_year The start year of the data
+#' @param end_year The end year of the data
+visualpanel_builder <- function(title,
+                                panel_header,
+                                panel_desc,
+                                panelid,
+                                start_year,
+                                end_year) {
+  choices <- choice_builder(panelid)
+  all_cards <- data_ui(panelid, choices, start_year, end_year)
   panel <- bslib::nav_panel(
     title = title,
     page_header_card(panel_header, panel_desc),
     bslib::card(
       bslib::card_title("Select Your Filters", class = "var-select-header"),
       title = "",
-      if (title %in% c("Number", "Assets", "Revenues", "Expenses", "Benefits", "Payroll Taxes")) {
+      if (title %in% c("Number",
+                       "Assets",
+                       "Revenues",
+                       "Expenses",
+                       "Benefits",
+                       "Payroll Taxes")) {
         bslib::layout_column_wrap(all_cards[["org_card"]], all_cards[["date_card"]], all_cards[["subsector_card"]], all_cards[["size_card"]], all_cards[["geo_card"]])
       } else if (title %in% c("Private Foundation Grants")) {
         bslib::layout_column_wrap(all_cards[["date_card"]], all_cards[["subsector_card"]], all_cards[["size_card"]], all_cards[["geo_card"]], )
@@ -21,5 +38,4 @@ visualpanel_builder <- function(title, panel_header, panel_desc, panelid, start_
   return(panel)
 }
 
-                 
-  
+
