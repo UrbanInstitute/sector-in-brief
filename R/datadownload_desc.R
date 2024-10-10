9c06ccdf0f2161700034f14b569c1f7f127b9413
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-10 15:14:46 -0400

    Function with data download dashboard description

---
+++ R/datadownload_desc.R
@@ -0,0 +1,7 @@
+data_download_subheader <- function(){
+  text <-
+    "Nonprofits are required to file tax returns with the Internal Revenue Service (IRS).\n
+     Since 1989, NCCS has been collecting these returns and making them available to the public.
+     This data download tool allows you to easily assemble panel data sets with data 
+     from tax filings made by nonprofits in the United States since 1989."
+}
