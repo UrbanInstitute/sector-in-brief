397dfd5b93a8a0b0198718ec772489b9b3a2fb7c
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-09 21:42:35 -0400

    First draft of welcome page layout

--- R/welcome.R
+++ R/welcome.R
@@ -4,30 +4,49 @@ welcome <- bslib::nav_panel(
   htmltools::div(
     class = "welcome-banner",
     htmltools::div(
-     class = "welcome-header",
-     "Welcome to The National Center of Charitable Statistic's Research Data Tool.
-      A research tool that allows you to both assemble and visualize panel data derived
-     directly from the IRS' Form 990.",
+      class = "welcome-header",
+     htmltools::h1(class = "text-3xl",
+                   "Welcome to the National Center for Charitable Statistic's", 
+                   htmltools::p(class = "text-3xl-cyan", "Sector In Brief")),
+     htmltools::p(class = "leading-relaxed",
+                   "A research tool for policymakers to visualize and download panel data 
+                    derived directly from the IRS' Form 990.")
     )
   ),
   htmltools::div(
-    class = "banner-dark",
+    class = "banner-light",
     htmltools::div(
-      class = "welcome-header",
-      htmltools::h2("Visualize Data"),
-      htmltools::p("Explore data on the number of nonprofits, nonprofit financials,
-                   private foundation grantmaking, and donor advised funds extracted
-                   directly from the Form 990.")
+      class = "flex-box--row",
+      htmltools::div(
+        class = "flex-box--column",
+        htmltools::h1(class = "header--md",
+                      "Visualize"),
+        htmltools::p(class = "subheader",
+                     htmltools::HTML("Visualize data on the <span style='color: #fdbf11'>
+                     number of nonprofits</span>, <span style='color: #ec008b'>
+                     nonprofit financials</span>,<span style='color: #55b748'>
+                   private foundation grantmaking</span>, and 
+                   <span style='color: #db2b27'>donor advised funds</span> extracted
+                   directly from the Form 990."))
+      ),
+      htmltools::img(class = "img-box",
+                     src = "ui-logo-rgb.png")
     )
   ),
   htmltools::div(
-    class = "banner-dark",
+    class = "banner-light",
     htmltools::div(
-      class = "welcome-header",
-      htmltools::h2("Download Data"),
-      htmltools::p("Explore data on the number of nonprofits, nonprofit financials,
-                   private foundation grantmaking, and donor advised funds extracted
-                   directly from the Form 990.")
+      class = "flex-box--row",
+      htmltools::div(
+        class = "flex-box--column",
+        htmltools::h1(class = "header--md",
+                      "Download"),
+        htmltools::p(class = "subheader",
+                     "Assemble custom panel data sets from NCCS' archive of 
+                      Form 990 data.")
+      ),
+      htmltools::img(class = "img-box",
+                     src = "ui-logo-rgb.png")
     )
   )
 )
