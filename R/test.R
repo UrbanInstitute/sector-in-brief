ba44c07066a5c4f55ef4fa213016c0fb3a57ff9a
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-16 15:54:15 -0400

    Add package libraries

--- R/test.R
+++ R/test.R
@@ -1,2 +1,11 @@
-# Some test code for Christina
-print("Hello World")
+######### Script Header ##################
+# Title: This script prints hello world
+# Programmer: Christina urban_email_here
+# Data created:
+# Date last modified: 
+#########################################
+
+
+# Load libraries
+library(dplyr)
+library(ggplot2)
