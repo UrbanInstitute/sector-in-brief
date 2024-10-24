d86ea59084f27f9bb6d914f926e514d26bb41308
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-24 00:41:10 -0400

    Refactor html code to internal objects

--- R/page_header_card.R
+++ R/page_header_card.R
@@ -2,7 +2,7 @@ page_header_card <- function(header, subheader) {
   card <- htmltools::div(
     class = "var-title-card",
     htmltools::h2(header, class = "var-header"),
-    htmltools::h3(subheader, class = "var-sub-header")
+    subheader
   )
   return(card)
 }
