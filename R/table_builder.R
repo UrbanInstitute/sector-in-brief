beff1fea143a1186c575ab01761f45bca15c6818
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-24 00:38:04 -0400

    Add full subsector choices, remove asset size 0 and add NA values for PF variables

--- R/table_builder.R
+++ R/table_builder.R
@@ -1,4 +1,4 @@
-table_builder <- function(data, groupby_var, groupby_var_2, sum_var) {
+table_builder <- function(data, groupby_var, groupby_var_2, sum_var, is_pf) {
   tryCatch({
     if (is.null(groupby_var_2)) {
       table <- data |>
@@ -7,31 +7,52 @@ table_builder <- function(data, groupby_var, groupby_var_2, sum_var) {
         dplyr::collect()
     } else {
       table <- data |>
+        na.omit() |>
         group_by(!!sym(groupby_var), !!sym(groupby_var_2)) |>
         summarise(!!sum_var := sum(!!sym(sum_var), na.rm = TRUE)) |>
         dplyr::collect()
       if (groupby_var_2 == "Asset Size") {
         table <- table |>
+          dplyr::filter(`Asset Size` != 0) |>
           dplyr::mutate(
-            "Asset Size" = case_when(
-              `Asset Size` <= 1 ~ "Under $100,000",
+            "Asset Size" = dplyr::case_when(
+              `Asset Size` == 1 ~ "Under $100,000",
               `Asset Size` == 2 ~ "$100,000 - $499,999",
               `Asset Size` == 3 ~ "$500,000 - $999,999",
               `Asset Size` == 4 ~ "$1 Million - $4.99 Million",
               `Asset Size` == 5 ~ "$5 Million - $9.99 Million",
-              `Asset Size` == 6 ~ "Above $10 Million",
+              `Asset Size` == 6 ~ "Above $10 Million"
+            )
+          ) |>
+          dplyr::collect()
+      } else if (groupby_var_2 == "Subsector"){
+        table <- table |>
+          dplyr::mutate(
+            "Subsector" = dplyr::case_when(
+              `Subsector` == "ART" ~ "Arts, Culture, and Humanities",
+              `Subsector` == "EDU" ~ "Education (minus Universities)",
+              `Subsector` == "HEL" ~ "Health (minus Hospitals)",
+              `Subsector` == "HMS" ~ "Human Services",
+              `Subsector` == "IFA" ~ "International, Foreign Affairs",
+              `Subsector` == "PSB" ~ "Public, Societal Benefit",
+              `Subsector` == "REL" ~ "Religion Related",
+              `Subsector` == "MMB" ~ "Mutual/Membership Benefit",
+              `Subsector` == "UNI" ~ "Universities",
+              `Subsector` == "HOS" ~ "Hospitals",
+              `Subsector` == "ENV" ~ "Environment and Animals",
+              `Subsector` == "UNU" ~ "Other"
             )
           ) |>
           dplyr::collect()
       }
     }
-    if (sum_var == "Total Contributions"){
+    if (is_pf == TRUE && groupby_var == "Tax Year"){
       table <- table |>
         dplyr::mutate(
-          `Total Contributions` = dplyr::if_else(
-            `Tax Year` %in% c(2016, 2017, 2018),
+          !!sym(sum_var) := dplyr::if_else(
+            `Tax Year` %in% c(2016, 2017, 2018, 2019),
             NA,
-            `Total Contributions`
+            !!sym(sum_var)
           )
         ) |>
         dplyr::collect()
