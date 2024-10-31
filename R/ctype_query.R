e0e847b7bac90417931d158d00236156d1f0fb81
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-31 02:01:02 -0400

    Update function for multiple ctype input values

--- R/ctype_query.R
+++ R/ctype_query.R
@@ -4,13 +4,14 @@
 #' @param ctype_level2 second level of organization type
 #' @return list of filters edited in place
 ctype_query <- function(filter_ls, ctype_level1, ctype_level2){
-  if (ctype_level1 == "501(c)(3)"){
-    filter_ls[["Organization Type"]] <- c("501(c)(3) Public Charities",
+  if (ctype_level1 %in% "501(c)(3)"){
+    filter_ls[["Organization Type"]] <- c(ctype_level1,
+                                          "501(c)(3) Public Charities",
                                           "501(c)(3) Private Foundations")
-  } else if (ctype_level1 == "501(c)(4) Social Welfare Organizations") {
-    filter_ls[["Organization Type"]] <- "501(c)(4)"
-  } else if (ctype_level1 == "Other Nonprofits") {
-    filter_ls[["Organization Type"]] <- ctype_level2
+  } else if (ctype_level1 %in% "501(c)(4) Social Welfare Organizations") {
+    filter_ls[["Organization Type"]] <- c(ctype_level1, "501(c)(4)")
+  } else if (ctype_level1 %in% "Other Nonprofits") {
+    filter_ls[["Organization Type"]] <- c(ctype_level1, ctype_level2)
   } else {
     filter_ls[["Organization Type"]] <- ctype_level1
   }
