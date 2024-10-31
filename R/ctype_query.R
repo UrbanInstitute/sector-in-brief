82497f89a264f4a67be2c1a6b835f8eec42b15f4
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-31 02:05:15 -0400

    Debug incorrect function logic

--- R/ctype_query.R
+++ R/ctype_query.R
@@ -4,13 +4,13 @@
 #' @param ctype_level2 second level of organization type
 #' @return list of filters edited in place
 ctype_query <- function(filter_ls, ctype_level1, ctype_level2){
-  if (ctype_level1 %in% "501(c)(3)"){
+  if ("501(c)(3)" %in% ctype_level1){
     filter_ls[["Organization Type"]] <- c(ctype_level1,
                                           "501(c)(3) Public Charities",
                                           "501(c)(3) Private Foundations")
-  } else if (ctype_level1 %in% "501(c)(4) Social Welfare Organizations") {
+  } else if ("501(c)(4) Social Welfare Organizations" %in% ctype_level1) {
     filter_ls[["Organization Type"]] <- c(ctype_level1, "501(c)(4)")
-  } else if (ctype_level1 %in% "Other Nonprofits") {
+  } else if ("Other Nonprofits" %in% ctype_level1) {
     filter_ls[["Organization Type"]] <- c(ctype_level1, ctype_level2)
   } else {
     filter_ls[["Organization Type"]] <- ctype_level1
