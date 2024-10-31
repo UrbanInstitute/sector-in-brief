79a53a981c8243f0da09b2d2aa2d7905e29e3e1f
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-31 02:05:01 -0400

    Debug incorrect function logic

--- R/caption_pf.R
+++ R/caption_pf.R
@@ -3,7 +3,7 @@
 #' @param ctype_level1 A character string indicating the level 1 organization type
 #' @return A character string with the plot caption edited in place
 caption_pf <- function(caption, ctype_level1) {
-  if (ctype_level1 %in% "501(c)(3) Private Foundations") {
+  if ("501(c)(3) Private Foundations" %in% ctype_level1) {
     caption <- paste(
       caption,
       "Organization Type: Private foundations are charitable organizations that typically receive most of their funding from a single source and primarily exist to make grants, rather than operate programs.",
