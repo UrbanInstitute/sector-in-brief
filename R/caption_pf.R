0f6250f34e38742bb49eda1a504981a20cdd2784
Thiyaghessan <thiyaghessan@gmail.com>
2024-10-31 01:58:59 -0400

    Remove NULL check for ctype with multiple inputs

--- R/caption_pf.R
+++ R/caption_pf.R
@@ -3,12 +3,12 @@
 #' @param ctype_level1 A character string indicating the level 1 organization type
 #' @return A character string with the plot caption edited in place
 caption_pf <- function(caption, ctype_level1) {
-  if (! is.null(ctype_level1)){
-    if (ctype_level1 == "501(c)(3) Private Foundations"){
-      caption <- paste(caption, 
-                        "Organization Type: Private foundations are charitable organizations that typically receive most of their funding from a single source and primarily exist to make grants, rather than operate programs.",
-                        "\n")
-    }
+  if (ctype_level1 %in% "501(c)(3) Private Foundations") {
+    caption <- paste(
+      caption,
+      "Organization Type: Private foundations are charitable organizations that typically receive most of their funding from a single source and primarily exist to make grants, rather than operate programs.",
+      "\n"
+    )
   }
   return(caption)
 }
