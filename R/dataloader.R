9d6b49f6047b0c2d00c53ebb121476948458d5cd
Thiyaghessan <thiyaghessan@gmail.com>
2024-11-05 19:58:57 -0500

    Add filter to remove erroneous DAF values

--- R/dataloader.R
+++ R/dataloader.R
@@ -2,6 +2,13 @@
 dataloader <- function(path, cols=NULL) {
   shinycssloaders::showPageSpinner()
   data_select <- arrow::read_parquet(path, as_data_frame = FALSE, col_select = cols)
+  if ("Number of DAFs" %in% cols){
+    data_select <- data_select |>
+      dplyr::filter(
+        `Number of DAFs` <= 50000
+      ) |>
+      dplyr::compute()
+  }
   shinycssloaders::hidePageSpinner()
   return(data_select)
 }
