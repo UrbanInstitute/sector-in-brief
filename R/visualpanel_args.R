d08a93c9eb0df6091d41142b06149308e8d668c8
Thiyaghessan <thiyaghessan@gmail.com>
2024-11-11 19:01:39 -0500

    Update DAF date slider to tax year 2021

--- R/visualpanel_args.R
+++ R/visualpanel_args.R
@@ -1,6 +1,6 @@
 visualpanel_args <- tibble::tribble(
   ~title, ~panel_header, ~panel_desc, ~panelid, ~start_year, ~end_year,
-  "Number", "Number of Nonprofits", number_of_nonprofits, "number", 1989, 2024,
+  "Numbers", "Number of Nonprofits", number_of_nonprofits, "number", 1989, 2024,
   "Assets", "Assets", assets_desc, "assets", 1989, 2021,
   "Revenues", "Revenues", revenue_desc, "revenues", 1989, 2021,
   "Expenses", "Expenses", expenses_desc, "expenses", 1989, 2021,
@@ -8,9 +8,9 @@ visualpanel_args <- tibble::tribble(
   "Payroll Taxes", "Payroll Taxes", payroll_desc, "payroll", 1989, 2021,
   "Private Foundation Grants", "Grants", grants_desc, "pf_amount", 1989, 2021,
   "Average Foundation Grantmaking", "Average Grantmaking", avg_grants_desc, "pf_avg", 1989, 2021,
-  "Number of DAFs", "Number of DAFs", daf_number_desc, "daf_number", 1989, 2021,
-  "DAF Contributions", "DAF Contributions", daf_contributions_desc,"daf_contributions", 1989, 2021,
-  "DAF Grants", "DAF Grants", daf_grants_desc, "daf_grants", 1989, 2021,
-  "DAF Value", "DAF Value", daf_value_desc, "daf_value", 1989, 2021,
-  "DAF Proportion", "Percentage of organizations that maintain a DAF", daf_proportion_desc, "daf_proportion", 1989, 2021,
+  "Number of DAFs", "Number of DAFs", daf_number_desc, "daf_number", 2021, 2021,
+  "DAF Contributions", "DAF Contributions", daf_contributions_desc,"daf_contributions", 2021, 2021,
+  "DAF Grants", "DAF Grants", daf_grants_desc, "daf_grants", 2021, 2021,
+  "DAF Value", "DAF Value", daf_value_desc, "daf_value", 2021, 2021,
+  "DAF Percentage", "Percentage of organizations that maintain a DAF", daf_proportion_desc, "daf_proportion", 2021, 2021,
 )
