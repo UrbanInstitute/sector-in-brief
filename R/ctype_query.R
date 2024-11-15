66e956c560ddcde9e1d7d6d8b2a370556ad6ca69
Thiyaghessan <thiyaghessan@gmail.com>
2024-11-15 18:41:33 -0500

    Refactor multiple ctype filters into single ctype filter

--- R/ctype_query.R
+++ R/ctype_query.R
@@ -1,19 +1,8 @@
 #' @title Function to create ctype query
 #' @param filter_ls list of filters
-#' @param ctype_level1 first level of organization type
-#' @param ctype_level2 second level of organization type
+#' @param ctype character scalar. 501(c) type.
 #' @return list of filters edited in place
-ctype_query <- function(filter_ls, ctype_level1, ctype_level2){
-  if ("501(c)(3)" %in% ctype_level1){
-    filter_ls[["Organization Type"]] <- c(ctype_level1,
-                                          "501(c)(3) Public Charities",
-                                          "501(c)(3) Private Foundations")
-  } else if ("501(c)(4) Social Welfare Organizations" %in% ctype_level1) {
-    filter_ls[["Organization Type"]] <- c(ctype_level1, "501(c)(4)")
-  } else if ("Other Nonprofits" %in% ctype_level1) {
-    filter_ls[["Organization Type"]] <- c(ctype_level1, ctype_level2)
-  } else {
-    filter_ls[["Organization Type"]] <- ctype_level1
-  }
+ctype_query <- function(filter_ls, ctype){
+  filter_ls[["Organization Type"]] <- ctype_id[ctype] |> unlist()
   return(filter_ls)
 }
