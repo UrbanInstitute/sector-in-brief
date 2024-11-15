80318e9ed002d470760a86148791702131973da3
Thiyaghessan <thiyaghessan@gmail.com>
2024-11-15 17:44:39 -0500

    Add text for the welcome page

--- R/text_welcome.R
+++ R/text_welcome.R
@@ -22,17 +22,17 @@ welcome_para_1 <- htmltools::p(
 
 welcome_para_2 <- htmltools::p(
   class = "subheader",
-  "Each year, nonprofits with over $50,000 in annual gross receipts complete the Internal Revenue Service 990 Tax Form and associated files, disclosing information to ensure the sector remains transparent and accountable to the public."
+  htmltools::HTML("The National Center for Charitable Statistics (NCCS) at the Urban Institute processes and publishes comprehensive nonprofit sector data to advance research and deepen public understanding of charitable organizations. NCCS data encompasses the entire spectrum of tax-exempt organizations, which includes 501(c)3 public charities and private foundations, 501(c)4 organizations, and all the additional <a href='https://www.irs.gov/charities-non-profits/exempt-organization-types'>501(c) classifications and subsectors</a>.")
 )
 
 welcome_para_3 <- htmltools::p(
   class = "subheader",
-  "Urban Institute’s National Center for Charitable Statistics (NCCS) cleans, processes, and publishes these complex public datasets accessible to support nonprofit research that strengthens the public’s understanding of the field."
+  "The Nonprofit Sector in Brief Data Explorer aggregates NCCS data to illuminate sector-wide trends in nonprofits over more than three decades. It allows users to view data and trends by organization type, subsector, asset size, geography, and time period rather than by a specific organization. Looking across a range of organizations provides a snapshot into the state of the nonprofit sector as told through data released by the IRS."
 )
 
 welcome_para_4 <- htmltools::p(
   class = "subheader",
-  "Powered by NCCS data, the Nonprofit Sector in Brief Explorer illuminates trends in the nonprofit sector over more than three decades. Users can drill down on a specific type of nonprofit organization, time period, size, subsector, or geography for localized insight."
+  "This data tool offers two ways to explore NCCS data for all nonprofits or for a customized subset of nonprofits:"
 )
 
 welcome_visual_header <- htmltools::h1(class = "header--md", "Visualize")
@@ -50,23 +50,6 @@ welcome_download_para <- htmltools::p(
   "Assemble custom panel data sets for download from NCCS's archive of cleaned and harmonized Form 990 data (the NCCS Core) and Business Master Files."
 )
 
-welcome_credits <- htmltools::div(
-  class = "footer_text",
-  htmltools::p(
-    "The Nonprofit Sector in Brief Data Explorer makes public data on nonprofits from 1989 to the present available to visualize and download."  
-  ),
-  htmltools::p("Research - Jesse Lecy, Hannah Martin, Laura Tomasko"),
-  htmltools::p("Development - Thiyaghessan Poongundranar"),
-  htmltools::p("Design - Thiyaghessan Poongundranar")
-)
-
-welcome_about <- htmltools::div(
-  class = "footer_text",
-  htmltools::HTML(
-    "<p>This project is funded by the National Center for Charitable Statistics (NCCS). The NCCS data used in this data explorer contains standardized names, data types, and definitions for all variables across the various iterations of the IRS Form 990. NCCS data links tax records from every nonprofit filed between 1989 to 2022 in a single time-series panel, complete with geocoding to the Census tract and block levels for easy filtering. For more information, see the <a href='https://nccs.urban.org/'>NCCS Website</a></p>"
-  )
-)
-
 usethis::use_data(
   welcome_title,
   welcome_subtitle,
@@ -78,8 +61,6 @@ usethis::use_data(
   welcome_visual_para,
   welcome_download_header,
   welcome_download_para,
-  welcome_credits,
-  welcome_about,
   overwrite = TRUE,
   internal = TRUE
 )
