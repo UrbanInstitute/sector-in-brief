934345da801590e4a697f82ae5268f869d1eca5d
Thiyaghessan <thiyaghessan@gmail.com>
2024-11-15 18:49:32 -0500

    Caption for plots containing private foundation data

--- R/caption_pf.R
+++ R/caption_pf.R
@@ -1,12 +1,14 @@
 #' @title Edit plot caption if plotting private foundations
 #' @param caption A character string containing the plot caption
-#' @param ctype_level1 A character string indicating the level 1 organization type
+#' @param ctype A character string indicating the 501c type
 #' @return A character string with the plot caption edited in place
-caption_pf <- function(caption, ctype_level1) {
-  if ("501(c)(3) Private Foundations" %in% ctype_level1) {
+caption_pf <- function(caption, ctype) {
+  if ("501(c)(3) Private Foundations" %in% ctype) {
     caption <- paste(
       caption,
       "Organization Type: Private foundations are charitable organizations that typically receive most of their funding from a single source and primarily exist to make grants, rather than operate programs.",
+      "\n",
+      "The IRS has not released 990 PF tax records for tax years 2016 – 2018, thus points from these years are represented with a dotted line to indicate their incompleteness.",
       "\n"
     )
   }
