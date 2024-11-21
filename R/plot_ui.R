04ee758f1ff14071b7d17ac84dc3072cfa662f72
Thiyaghessan <thiyaghessan@gmail.com>
2024-11-21 15:01:27 -0500

    Update .css styling for card title

--- R/plot_ui.R
+++ R/plot_ui.R
@@ -1,9 +1,8 @@
 plot_ui <- function(id, selected_geographies) {
   plotpanels <- plotpanel_mapper(plotpanel_args, id)
-  bslib::navset_card_tab(
-    title =   "",
+  bslib::navset_card_underline(
+    title =   "Visualize Your Results",
     height = "100%",
-    bslib::card_title("Visualize Your Results", class = "viz-header"),
     plotpanels[["Overall"]],
     plotpanels[["By Organization Type"]],
     plotpanels[["By Subsector"]],
