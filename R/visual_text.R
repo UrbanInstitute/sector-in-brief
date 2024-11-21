db27e4b1fad13c3e49bafe5894d5f40a4298ca75
Thiyaghessan <thiyaghessan@gmail.com>
2024-11-21 14:57:33 -0500

    Update text and styling with COMM edits

--- R/visual_text.R
+++ R/visual_text.R
@@ -1,46 +1,44 @@
 # This script contains the text that goes into the Data Visualizations page
-
 number_of_nonprofits <- htmltools::tagList(
   htmltools::p(
     "Understanding the number of nonprofit organizations registered with the IRS provides a sense of the overall size and growth of the sector. NCCS data encompass all tax-exempt ",
-    htmltools::a(href = "https://www.irs.gov/charities-non-profits/exempt-organization-types", "501(c) classifications and subsectors"),
-    ", including 501(c)(3) public charities and private foundations and 501(c)(4) organizations."
+    htmltools::a(href = "https://www.irs.gov/charities-non-profits/exempt-organization-types", "501(c) classifications and subsectors,"),
+    "including 501(c)(3) public charities and private foundations and 501(c)(4) organizations."
   ),
   
   htmltools::p(
-    "The chart below shows financial data for the nonprofit sector overall. Select the nonprofit sizes, geographies, and date range you're interested in to create a custom chart. Use the tabs above the chart to view your selections broken out by organization type, subsector, geography, or asset size."
+    "The chart below shows the number of organizations for the nonprofit sector overall. Select the nonprofit sizes, geographies, and date range you're interested in to create a custom chart. Use the tabs above the chart to view your selections broken out by organization type, subsector, geography, or asset size."
   ),
   
   htmltools::p(
     class = "base",
-    htmltools::tags$b("Data Source"),
-    ": NCCS's ",
-    htmltools::a(href = "https://urbaninstitute.github.io/nccs/datasets/bmf/", "Unified Business Master Files (BMF)"),
-    ", which contains data processed from IRS BMF for calendar years 1989 through 2024. This includes all nonprofits, even those with gross receipts less than or equal to $50,000.",
-    htmltools::a(href = "https://nccsdata.s3.us-east-1.amazonaws.com/dataexplorer/visuals/data_dictionary.xlsx", "Download Data Dictionary")
+    htmltools::tags$b("Data Source:"),
+    "NCCS's ",
+    htmltools::a(href = "https://urbaninstitute.github.io/nccs/datasets/bmf/", "Unified Business Master File,"),
+    "which contains data processed from IRS Business Master Files for calendar years 1989 through 2024. This includes all nonprofits, even those with gross receipts less than or equal to $50,000.",
+    htmltools::a(href = "https://nccsdata.s3.us-east-1.amazonaws.com/dataexplorer/visuals/data_dictionary.xlsx", "Download data dictionary.")
   )
 )
 
 finance_header <-
   htmltools::tagList(
-    htmltools::h4("Finances"),
+    htmltools::h2("Finances"),
     htmltools::p(
       "The financial state of nonprofits reflects the resources available to nonprofits for meeting their missions. NCCS data encompass all tax-exempt ",
-      htmltools::a(href = "https://www.irs.gov/charities-non-profits/exempt-organization-types", "501(c) classifications and subsectors"),
-      ", including 501(c)3 public charities and private foundations and 501(c)4 organizations."
+      htmltools::a(href = "https://www.irs.gov/charities-non-profits/exempt-organization-types", "501(c) classifications and subsectors,"),
+      "including 501(c)3 public charities and private foundations and 501(c)4 organizations."
     ),
     htmltools::p(
       "Select a financial metric to view, and then use filters to customize the graph by organization type, subsector, asset size, geography, or data range. Use the tabs to change the view from overall data to view by subsector, geography, or asset size."
     ),
     htmltools::p(
       class = "base",
-      htmltools::tags$b("Data Source"),
-      ": ",
-      htmltools::a(href = "https://lecy.github.io/nccs/datasets/core/", "NCCS' Core Series"),
-      ", which contains data processed from the IRS' ",
-      htmltools::a(href = "https://www.irs.gov/statistics/soi-tax-stats-annual-extract-of-tax-exempt-organization-financial-data", "Statistics of Income (SOI) Extracts"),
-      " Extracts for Form 990, 990-EZ and 990-PF for tax years 1989-2021. This panel does not include 990-N data and thus excludes small nonprofits with gross receipts less than or equal to $50,000. More information can be found in the About page. ",
-      htmltools::a(href = "https://nccsdata.s3.us-east-1.amazonaws.com/dataexplorer/visuals/data_dictionary.xlsx", "Download Data Dictionary")
+      htmltools::tags$b("Data Source:"),
+      htmltools::a(href = "https://lecy.github.io/nccs/datasets/core/", "NCCS's Core Series,"),
+      "which contains data processed from the IRS's ",
+      htmltools::a(href = "https://www.irs.gov/statistics/soi-tax-stats-annual-extract-of-tax-exempt-organization-financial-data", "Statistics of Income (SOI) extracts"),
+      "for Forms 990, 990-EZ and 990-PF for tax years 1989 through 2021. This panel does not include 990-N data and thus excludes small nonprofits with gross receipts less than or equal to $50,000. More information can be found in the About page.",
+      htmltools::a(href = "https://nccsdata.s3.us-east-1.amazonaws.com/dataexplorer/visuals/data_dictionary.xlsx", "Download data dictionary.")
     )
   )
 
@@ -57,11 +55,11 @@ expenses_desc <- htmltools::p(
 )
 
 benefits_desc <- htmltools::p(
-  "Total benefits are the sum of salaries, wages, benefits, pension plan accruals and contributions, and the 401(k) and 403(n) contributions nonprofits pay to and on behalf of employees. It provides a measure of overall spending on nonprofit-sector employees."
+  "Total benefits are the sum of salaries, wages, benefits, pension plan accruals and contributions, and the 401(k) and 403(b) contributions nonprofits pay to and on behalf of employees. It provides a measure of overall spending on nonprofit-sector employees."
 )
 
 pf_header <- htmltools::tagList(
-  htmltools::h4("Private Foundation Grantmaking"),
+  htmltools::h2("Private Foundation Grantmaking"),
   htmltools::tagList(
     htmltools::p(
       "Grants by private foundations, which are typically funded by an individual donor or family, are one of several sources of private funding available to public charities. Trends in these foundations’ grantmaking reflect their available resources."
@@ -78,9 +76,9 @@ pf_header <- htmltools::tagList(
   ),
   htmltools::p(
     class = "base",
-    htmltools::tags$b("Data Source"),
-    ": NCCS's Core Series, which contains data processed from the IRS's Statistics of Income Extracts for Form 990-PF for tax years 1989 through 2021. ",
-    htmltools::a(href = "https://nccsdata.s3.us-east-1.amazonaws.com/dataexplorer/visuals/data_dictionary.xlsx", "Download Data Dictionary")
+    htmltools::tags$b("Data Source:"),
+    "NCCS's Core Series, which contains data processed from the IRS's Statistics of Income Extracts for Form 990-PF for tax years 1989 through 2021.",
+    htmltools::a(href = "https://nccsdata.s3.us-east-1.amazonaws.com/dataexplorer/visuals/data_dictionary.xlsx", "Download data dictionary.")
   )
 )
 
@@ -89,10 +87,10 @@ grants_desc <- htmltools::p(
 )
 
 daf_header <- htmltools::tagList(
-  htmltools::h4("Donor Advised Funds"),
+  htmltools::h2("Donor-Advised Funds"),
   htmltools::p(
     "A ",
-    htmltools::a(href = "https://www.irs.gov/charities-non-profits/charitable-organizations/donor-advised-funds", "Donor Advised Fund"),
+    htmltools::a(href = "https://www.irs.gov/charities-non-profits/charitable-organizations/donor-advised-funds", "donor-advised fund"),
     " (DAF) is a tool that allows individuals and organizations to contribute money and noncash assets to a giving account, receive an immediate tax deduction, and recommend grants to nonprofits at a later time. Public charities, which are the only type of nonprofits that hold DAFs, can have as many individual DAF accounts as they want. This section summarizes key trends in the use of DAFs as a giving tool."
   ),
   htmltools::p(
@@ -103,11 +101,10 @@ daf_header <- htmltools::tagList(
   ),
   htmltools::p(
     class = "base",
-    htmltools::tags$b("Data Source"),
-    ": ",
-    htmltools::a(href = "https://lecy.github.io/nccs/datasets/efile/", "NCCS' E-Filer Catalog"),
-    ", which contains data processed from Schedule D for e-filed IRS Form 990 in tax year 2021. This panel does not include 990-N data and thus excludes small nonprofits with gross receipts less than or equal to $50,000. ",
-    htmltools::a(href = "https://nccsdata.s3.us-east-1.amazonaws.com/dataexplorer/visuals/data_dictionary.xlsx", "Download Data Dictionary")
+    htmltools::tags$b("Data Source:"),
+    htmltools::a(href = "https://lecy.github.io/nccs/datasets/efile/", "NCCS' E-Filer Catalog,"),
+    "which contains data processed from Schedule D for e-filed IRS Form 990 in tax year 2021. This panel does not include 990-N data and thus excludes small nonprofits with gross receipts less than or equal to $50,000. ",
+    htmltools::a(href = "https://nccsdata.s3.us-east-1.amazonaws.com/dataexplorer/visuals/data_dictionary.xlsx", "Download data dictionary.")
   )
 )
 
@@ -132,7 +129,7 @@ daf_grants_desc <-
   )
 
 daf_proportion_desc <- htmltools::p(
-  "The percentage of nonprofits that sponsor DAFs, defined as holding and operating DAF accounts, captures the prevalence of DAFs among public charities."
+  "Percentage of nonprofits that sponsor DAFs, defined as holding and operating DAF accounts, captures the prevalence of DAFs among public charities."
 )
 
 usethis::use_data(
