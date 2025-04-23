ee18ed4579b68e153a92f0466f3c942a2a6d4e80
Thiyaghessan <thiyaghessan@gmail.com>
2025-04-23 12:34:39 -0400

    Compute size by expense

--- R/plotpanel_args.R
+++ R/plotpanel_args.R
@@ -5,5 +5,5 @@ plotpanel_args <-
     "By Organization Type", "plot_ctype", "table_ctype", "dl_ctype", "table_ctype_title",
     "By Subsector", "plot_subsector", "table_subsector", "dl_subsector", "table_subsector_title",
     "By Geography", "plot_geo", "table_geo", "dl_geo", "table_geo_title",
-    "By Asset Size", "plot_size", "table_size", "dl_size", "table_size_title",
+    "By Size", "plot_size", "table_size", "dl_size", "table_size_title",
   )
