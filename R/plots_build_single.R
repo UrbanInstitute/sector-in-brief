dbccb36739c9f5c9ad501a6e1c709373ec148faa
Thiyaghessan <thiyaghessan@gmail.com>
2025-04-23 12:34:16 -0400

    update styling for readability

--- R/plots_build_single.R
+++ R/plots_build_single.R
@@ -9,54 +9,64 @@ plots_build_single <- function(table,
                                year_var) {
   if (length(unique(table[[year_var]])) > 1) {
     if (is.null(groupby_var)) {
-      single_line_plot(table = table, 
-                       title = title, 
-                       caption = caption, 
-                       yvar = yvar, 
-                       xvar = xvar, 
-                       ytitle = ytitle, 
-                       xtitle = xtitle)
+      single_line_plot(
+        table = table,
+        title = title,
+        caption = caption,
+        yvar = yvar,
+        xvar = xvar,
+        ytitle = ytitle,
+        xtitle = xtitle
+      )
     }
     else {
-      group_line_plot(table = table,
-                      groupby_var = groupby_var,
-                      title = title, 
-                      caption = caption, 
-                      yvar = yvar, 
-                      xvar = xvar, 
-                      ytitle = ytitle, 
-                      xtitle = xtitle,
-                      num_groups = unique(table[[groupby_var]]) |> length()) 
+      group_line_plot(
+        table = table,
+        groupby_var = groupby_var,
+        title = title,
+        caption = caption,
+        yvar = yvar,
+        xvar = xvar,
+        ytitle = ytitle,
+        xtitle = xtitle,
+        num_groups = unique(table[[groupby_var]]) |> length()
+      )
     }
   } else {
     if (is.null(groupby_var)) {
-      single_col_plot(table = table, 
-                      title = title, 
-                      caption = caption, 
-                      yvar = yvar, 
-                      xvar = xvar, 
-                      ytitle = ytitle, 
-                      xtitle = xtitle)
+      single_col_plot(
+        table = table,
+        title = title,
+        caption = caption,
+        yvar = yvar,
+        xvar = xvar,
+        ytitle = ytitle,
+        xtitle = xtitle
+      )
     }
     else if (length(unique(table[[groupby_var]])) == 1) {
-      single_col_plot(table = table, 
-                      title = title, 
-                      caption = caption, 
-                      yvar = yvar, 
-                      xvar = xvar, 
-                      ytitle = ytitle, 
-                      xtitle = xtitle)
+      single_col_plot(
+        table = table,
+        title = title,
+        caption = caption,
+        yvar = yvar,
+        xvar = xvar,
+        ytitle = ytitle,
+        xtitle = xtitle
+      )
     }
     else {
-      group_col_plot(table = table,
-                     groupby_var = groupby_var,
-                     title = title, 
-                     caption = caption, 
-                     yvar = yvar, 
-                     xvar = xvar, 
-                     ytitle = ytitle, 
-                     xtitle = xtitle,
-                     num_groups = unique(table[[groupby_var]]) |> length())
+      group_col_plot(
+        table = table,
+        groupby_var = groupby_var,
+        title = title,
+        caption = caption,
+        yvar = yvar,
+        xvar = xvar,
+        ytitle = ytitle,
+        xtitle = xtitle,
+        num_groups = unique(table[[groupby_var]]) |> length()
+      )
     }
   }
 }
